e7259cfa26b41d392c03451d398e969f72665a64
Phillipe Roger Souza <phillrog@hotmail.com>
2021-01-21 22:52:48 -0300

    Emitindo JWT pelo controller

--- src/services/NSE.Identidade.API/Controllers/AuthController.cs
+++ src/services/NSE.Identidade.API/Controllers/AuthController.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using NSE.Identidade.API.Extensions;
 using NSE.Identidade.API.Models;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace NSE.Identidade.API.Controllers
@@ -14,11 +20,13 @@ namespace NSE.Identidade.API.Controllers
 	{
 		private readonly SignInManager<IdentityUser> _signInManager;
 		private readonly UserManager<IdentityUser> _userManager;
+		private readonly AppSettings _appSettings;
 
-		public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+		public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager, IOptions<AppSettings> appSettings)
 		{
 			_signInManager = signInManager;
 			_userManager = userManager;
+			_appSettings = appSettings.Value;
 		}
 
 		[HttpPost("nova-conta")]
@@ -37,7 +45,7 @@ namespace NSE.Identidade.API.Controllers
 
 			if (result.Succeeded) {
 				await _signInManager.SignInAsync(user, false);
-				return Ok();
+				return Ok(GerarJwt(usuarioRegistro.Email));
 			}
 
 			return BadRequest();
@@ -51,10 +59,76 @@ namespace NSE.Identidade.API.Controllers
 			var result = await _signInManager.PasswordSignInAsync(usuarioLogin.Email, usuarioLogin.Senha, false, true);
 
 			if (result.Succeeded) {
-				return Ok();
+				return Ok(GerarJwt(usuarioLogin.Email));
 			}
 
 			return BadRequest();
 		}
+
+		private async Task<UsuarioRespostaLogin> GerarJwt(string email)
+		{
+			var user = await _userManager.FindByEmailAsync(email);
+			var claims = await _userManager.GetClaimsAsync(user);
+
+			var identityClaims = await ObterClaimsUsuario(claims, user);
+			var encodedToken = CodificarToken(identityClaims);
+
+			return ObterRespostaToken(encodedToken, user, claims);
+		}
+
+
+		private async Task<ClaimsIdentity> ObterClaimsUsuario(ICollection<Claim> claims, IdentityUser user)
+		{
+			var userRoles = await _userManager.GetRolesAsync(user);
+
+			claims.Add(new Claim(JwtRegisteredClaimNames.Sub, user.Id));
+			claims.Add(new Claim(JwtRegisteredClaimNames.Email, user.Email));
+			claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+			claims.Add(new Claim(JwtRegisteredClaimNames.Nbf, ToUnixEpochDate(DateTime.UtcNow).ToString()));
+			claims.Add(new Claim(JwtRegisteredClaimNames.Iat, ToUnixEpochDate(DateTime.UtcNow).ToString(), ClaimValueTypes.Integer64));
+			foreach (var userRole in userRoles)
+			{
+				claims.Add(new Claim("role", userRole));
+			}
+
+			var identityClaims = new ClaimsIdentity();
+			identityClaims.AddClaims(claims);
+
+			return identityClaims;
+		}
+
+		private string CodificarToken(ClaimsIdentity identityClaims)
+		{
+			var tokenHandler = new JwtSecurityTokenHandler();
+			var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+			var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
+			{
+				Issuer = _appSettings.Emissor,
+				Audience = _appSettings.ValidoEm,
+				Subject = identityClaims,
+				Expires = DateTime.UtcNow.AddHours(_appSettings.ExpiracaoHoras),
+				SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+			});
+
+			return tokenHandler.WriteToken(token);
+		}
+
+		private UsuarioRespostaLogin ObterRespostaToken(string encodedToken, IdentityUser user, IEnumerable<Claim> claims)
+		{
+			return new UsuarioRespostaLogin
+			{
+				AccessToken = encodedToken,
+				ExpiresIn = TimeSpan.FromHours(_appSettings.ExpiracaoHoras).TotalSeconds,
+				UsuarioToken = new UsuarioToken
+				{
+					Id = user.Id,
+					Email = user.Email,
+					Claims = claims.Select(c => new UsuarioClaim { Type = c.Type, Value = c.Value })
+				}
+			};
+		}
+
+		private static long ToUnixEpochDate(DateTime date)
+			=> (long)Math.Round((date.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
 	}
 }
--- src/services/NSE.Identidade.API/Models/UsuarioRegistro.cs
+++ src/services/NSE.Identidade.API/Models/UsuarioRegistro.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace NSE.Identidade.API.Models
 {
@@ -25,5 +26,26 @@ namespace NSE.Identidade.API.Models
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
         public string Senha { get; set; }
+    }
+
+
+    public class UsuarioRespostaLogin
+    {
+        public string AccessToken { get; set; }
+        public double ExpiresIn { get; set; }
+        public UsuarioToken UsuarioToken { get; set; }
+    }
+
+    public class UsuarioToken
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<UsuarioClaim> Claims { get; set; }
+    }
+
+    public class UsuarioClaim
+    {
+        public string Value { get; set; }
+        public string Type { get; set; }
     }
 }
