3216bf00b6788a42637a66648466230e2eb59329
Phillipe Roger Souza <phillrog@hotmail.com>
2021-01-23 17:25:39 -0300

    COnfigurado cookie e separado responsabilidades  de configuração

---
+++ src/web/NSE.WebApp.MVC/Configurations/IdentityConfig.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NSE.WebApp.MVC.Configurations
+{
+	public static class IdentityConfig
+	{
+		public static IServiceCollection AddIndetityConfiguration(this IServiceCollection services)
+		{
+			services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+				.AddCookie(options =>
+				{
+					options.LoginPath = "/login";
+					options.AccessDeniedPath = "/acesso-negado";
+				});
+			return services;
+		}
+
+		public static IApplicationBuilder UseIndetityConfiguration(this IApplicationBuilder app)
+		{
+			app.UseAuthentication();
+			app.UseAuthorization();
+			return app;
+		}
+	}
+}
---
+++ src/web/NSE.WebApp.MVC/Configurations/WebAppConfig.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace NSE.WebApp.MVC.Configurations
+{
+	public static class WebAppConfig
+	{
+		public static IServiceCollection AddWebAppConfiguration(this IServiceCollection services)
+		{
+			services.AddControllersWithViews();
+			return services;
+		}
+
+		public static IApplicationBuilder UseWebAppConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+				app.UseHsts();
+			}
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseIndetityConfiguration();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllerRoute(
+					name: "default",
+					pattern: "{controller=Home}/{action=Index}/{id?}");
+			});
+			
+			return app;
+		}
+	}
+}
