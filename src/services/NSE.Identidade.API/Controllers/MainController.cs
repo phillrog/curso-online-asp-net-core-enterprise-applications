d25ff85f7f0bf8f1e1aa9d83c76a0f190b0fceb9
Phillipe Roger Souza <phillrog@hotmail.com>
2021-01-22 22:57:50 -0300

    Criado a classe para customizar mensagens do identity

--- src/services/NSE.Identidade.API/Controllers/MainController.cs
+++ src/services/NSE.Identidade.API/Controllers/MainController.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace NSE.Identidade.API.Controllers
 {
