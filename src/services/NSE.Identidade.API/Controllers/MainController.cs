9fcad6b341343eddf38dfa6c5c188e38c8946c4b
Phillipe Roger Souza <phillrog@hotmail.com>
2021-01-22 22:26:18 -0300

    Inicio da customização de resposta

---
+++ src/services/NSE.Identidade.API/Controllers/MainController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Identidade.API.Controllers
+{
+	public abstract class MainController : ControllerBase
+	{
+		protected ICollection<string> Erros = new List<string>();
+
+		protected ActionResult CustomResponse(object result = null)
+		{
+			if (OperacaoValida())
+			{
+				return Ok();
+			}
+
+			return BadRequest(new ValidationProblemDetails(new Dictionary<string, string[]> {
+				{ "Mensagens"  , Erros.ToArray() }
+			}));
+		}
+
+		private bool OperacaoValida()
+		{
+			return !Erros.Any();
+		}
+	}
+}
