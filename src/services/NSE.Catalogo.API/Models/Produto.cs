8db0e7b55719cb3211c6333af8845794baf0ed3f
Phillipe Roger Souza <phillrog@hotmail.com>
2021-01-24 13:29:01 -0300

    Modelando a api

---
+++ src/services/NSE.Catalogo.API/Models/Produto.cs
@@ -0,0 +1,16 @@
+﻿using NSE.Core.DomainObjects;
+using System;
+
+namespace NSE.Catalogo.API.Models
+{
+	public class Produto : Entity
+	{
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public bool Ativo { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public string Imagem { get; set; }
+        public int QuantidadeEstoque { get; set; }
+    }
+}
