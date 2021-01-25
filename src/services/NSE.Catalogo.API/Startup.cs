41a9ec4e29f5529f11706d333dcdfdef216ada0f
Phillipe Roger Souza <phillrog@hotmail.com>
2021-01-25 20:58:10 -0300

    Configuração do swagger

--- src/services/NSE.Catalogo.API/Startup.cs
+++ src/services/NSE.Catalogo.API/Startup.cs
@@ -32,12 +32,15 @@ namespace NSE.Catalogo.API
 		{
 			services.AddApiConfiguration(Configuration);
 
+			services.AddSwaggerGen();
+
 			services.RegistrarServices();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
+			app.UseSwaggerConfiguration();
 			app.UseApiConfiguration(env);
 		}
 	}
