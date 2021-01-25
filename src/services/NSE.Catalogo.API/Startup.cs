63c730479239f0d19ea28975c299bc1ac28fea8f
Phillipe Roger Souza <phillrog@hotmail.com>
2021-01-25 20:54:16 -0300

    Configuração do ambiente e das dependencias

--- src/services/NSE.Catalogo.API/Startup.cs
+++ src/services/NSE.Catalogo.API/Startup.cs
@@ -2,29 +2,37 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using NSE.Catalogo.API.Data;
-using NSE.Catalogo.API.Data.Repository;
-using NSE.Catalogo.API.Models;
 using NSE.Catalogo.API.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace NSE.Catalogo.API
 {
 	public class Startup
 	{
-		public Startup(IConfiguration configuration)
-		{
-			Configuration = configuration;
-		}
-
 		public IConfiguration Configuration { get; }
+
+		public Startup(IHostEnvironment hostEnvironment)
+		{
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(hostEnvironment.ContentRootPath)
+				.AddJsonFile("appsettings.json", true, true)
+				.AddJsonFile($"appsettings.{hostEnvironment.EnvironmentName}.json", true, true)
+				.AddEnvironmentVariables();
+
+			if (hostEnvironment.IsDevelopment())
+			{
+				builder.AddUserSecrets<Startup>();
+			}
+
+			Configuration = builder.Build();
+		}
 
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddApiConfiguration(Configuration);
 
-			services.AddScoped<IProdutoRepository, ProdutoRepository>();
-			services.AddScoped<CatalogoContext>();
+			services.RegistrarServices();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
