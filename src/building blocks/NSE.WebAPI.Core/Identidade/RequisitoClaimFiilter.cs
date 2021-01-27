52fa4248962ef857a3e441c38eff5911dedb0dc2
Phillipe Roger Souza <phillrog@hotmail.com>
2021-01-26 22:15:44 -0300

    Filtro atributo para validar claim

---
+++ src/building blocks/NSE.WebAPI.Core/Identidade/RequisitoClaimFiilter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+
+namespace NSE.WebAPI.Core.Identidade
+{
+	public class RequisitoClaimFiilter : IAuthorizationFilter
+	{
+		private readonly Claim _claim;
+
+		public RequisitoClaimFiilter(Claim claim)
+		{
+			_claim = claim;
+		}
+		public void OnAuthorization(AuthorizationFilterContext context)
+		{
+			if (!context.HttpContext.User.Identity.IsAuthenticated)
+			{
+				context.Result = new StatusCodeResult(401);
+				return;
+			}
+
+			if (!CustomAuthorization.ValidarClaimsUsuario(context.HttpContext, _claim.Type, _claim.Value))
+			{
+				context.Result = new StatusCodeResult(403);
+				return;
+			}
+		}
+	}
+}
