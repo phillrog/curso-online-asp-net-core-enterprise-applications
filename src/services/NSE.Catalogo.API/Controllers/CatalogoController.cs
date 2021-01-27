f667355cfd03c0eb1927e0ea8640588834560153
Phillipe Roger Souza <phillrog@hotmail.com>
2021-01-26 22:25:40 -0300

    Implementando a validação do claim

--- src/services/NSE.Catalogo.API/Controllers/CatalogoController.cs
+++ src/services/NSE.Catalogo.API/Controllers/CatalogoController.cs
@@ -1,11 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using NSE.Catalogo.API.Models;
+using NSE.WebAPI.Core.Identidade;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace NSE.Catalogo.API.Controllers
 {
+    [ApiController]
+    [Authorize]
 	public class CatalogoController: Controller
 	{
         private readonly IProdutoRepository _produtoRepository;
@@ -16,13 +20,14 @@ namespace NSE.Catalogo.API.Controllers
         }
 
 
+        [AllowAnonymous]
         [HttpGet("catalogo/produtos")]
         public async Task<IEnumerable<Produto>> Index()
         {
             return await _produtoRepository.ObterTodos();
         }
 
-
+        [ClaimsAuthorize("Catalogo", "Ler")]
         [HttpGet("catalogo/produtos/{id}")]
         public async Task<Produto> ProdutoDetalhe(Guid id)
         {
