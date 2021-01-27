72c60e56cb69eecec2d909df95d1d97ba3b32de5
Phillipe Roger Souza <phillrog@hotmail.com>
2021-01-26 22:09:24 -0300

    Classe para validar claim do usuario

---
+++ src/building blocks/NSE.WebAPI.Core/Identidade/CustomAuthorization.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Linq;
+
+namespace NSE.WebAPI.Core.Identidade
+{
+	public class CustomAuthorization
+	{
+		public static bool ValidarClaimsUsuario(HttpContext context, string claimName, string claimValue)
+		{
+			return context.User.Identity.IsAuthenticated &&
+				context.User.Claims.Any(c => c.Type == claimName && claimValue.Contains(c.Value )) ;
+		}
+	}
+}
