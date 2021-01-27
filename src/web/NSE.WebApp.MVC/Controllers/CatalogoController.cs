2fc4654a562a639aac125665a584cf792aa550d4
Phillipe Roger Souza <phillrog@hotmail.com>
2021-01-26 22:49:04 -0300

    Foi criado o controller produto no mvc

---
+++ src/web/NSE.WebApp.MVC/Controllers/CatalogoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Controllers
+{
+	public class CatalogoController : MainController
+	{
+        [HttpGet]
+        [Route("")]
+        [Route("vitrine")]
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("produto-detalhe/{id}")]
+        public async Task<IActionResult> ProdutoDetalhe(Guid id)
+        {
+            return View();
+        }
+    }
+}
