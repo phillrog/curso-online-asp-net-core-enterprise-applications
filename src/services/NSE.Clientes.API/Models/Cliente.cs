c2f2b558389942292cfa346b632f31abdb6f87cb
Phillipe Roger Souza <phillrog@hotmail.com>
2021-01-28 23:08:46 -0300

    Definindo entidades

---
+++ src/services/NSE.Clientes.API/Models/Cliente.cs
@@ -0,0 +1,29 @@
+﻿using NSE.Core.Data;
+using NSE.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Clientes.API.Models
+{
+    public class Cliente : Entity, IAggregateRoot
+    {
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+        public bool Excluido { get; private set; }
+        public Endereco Endereco { get; private set; }
+
+        // EF Relation
+        protected Cliente() { }
+
+        public Cliente(string nome, string email, string cpf)
+        {
+            Nome = nome;
+            Email = email;
+            Cpf = cpf;
+            Excluido = false;
+        }
+    }
+}
---
+++ src/services/NSE.Clientes.API/Models/Endereco.cs
@@ -0,0 +1,31 @@
+﻿using NSE.Core.Data;
+using System;
+
+namespace NSE.Clientes.API.Models
+{
+	public class Endereco : Entity
+    {
+        public string Logradouro { get; private set; }
+        public string Numero { get; private set; }
+        public string Complemento { get; private set; }
+        public string Bairro { get; private set; }
+        public string Cep { get; private set; }
+        public string Cidade { get; private set; }
+        public string Estado { get; private set; }
+        public Guid ClienteId { get; private set; }
+
+        // EF Relation
+        public Cliente Cliente { get; protected set; }
+
+        public Endereco(string logradouro, string numero, string complemento, string bairro, string cep, string cidade, string estado)
+        {
+            Logradouro = logradouro;
+            Numero = numero;
+            Complemento = complemento;
+            Bairro = bairro;
+            Cep = cep;
+            Cidade = cidade;
+            Estado = estado;
+        }
+    }
+}
