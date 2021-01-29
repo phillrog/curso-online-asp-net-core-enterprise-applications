611820516f21f787d172f7a02cc8d13d651ce872
Phillipe Roger Souza <phillrog@hotmail.com>
2021-01-29 20:19:37 -0300

    Foi finalizado a raiz de agregação cliente

--- src/services/NSE.Clientes.API/Models/Cliente.cs
+++ src/services/NSE.Clientes.API/Models/Cliente.cs
@@ -7,23 +7,34 @@ using System.Threading.Tasks;
 
 namespace NSE.Clientes.API.Models
 {
-    public class Cliente : Entity, IAggregateRoot
-    {
-        public string Nome { get; private set; }
-        public string Email { get; private set; }
-        public Cpf Cpf { get; private set; }
-        public bool Excluido { get; private set; }
-        public Endereco Endereco { get; private set; }
+	public class Cliente : Entity, IAggregateRoot
+	{
+		public string Nome { get; private set; }
+		public Email Email { get; private set; }
+		public Cpf Cpf { get; private set; }
+		public bool Excluido { get; private set; }
+		public Endereco Endereco { get; private set; }
 
-        // EF Relation
-        protected Cliente() { }
+		// EF Relation
+		protected Cliente() { }
 
-        public Cliente(string nome, string email, Cpf cpf)
-        {
-            Nome = nome;
-            Email = email;
-            Cpf = cpf;
-            Excluido = false;
-        }
-    }
+		public Cliente(Guid id, string nome, string email, string cpf)
+		{
+			Id = id;
+			Nome = nome;
+			Email = new Email(email);
+			Cpf = new Cpf(cpf);
+			Excluido = false;
+		}
+
+		public void TrocaEmail(string email)
+		{
+			Email = new Email(email);
+		}
+
+		public void AtribuireEndereco(Endereco endereco)
+		{
+			Endereco = endereco;
+		}
+	}
 }
