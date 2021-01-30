46c004a74f8238d99850155564bef3de6f080c38
Phillipe Roger Souza <phillrog@hotmail.com>
2021-01-30 16:23:47 -0300

    Implementando chamada do cliente repository dentro do comando cliente

--- src/services/NSE.Clientes.API/Application/Commands/ClienteCommandHandler.cs
+++ src/services/NSE.Clientes.API/Application/Commands/ClienteCommandHandler.cs
@@ -7,13 +7,28 @@ using System.Threading.Tasks;
 
 namespace NSE.Clientes.API.Application.Commands
 {
-	public class ClienteCommandHandler : CommandHandler,  IRequestHandler<RegistrarClienteCommand, ValidationResult>
+	public class ClienteCommandHandler : CommandHandler, IRequestHandler<RegistrarClienteCommand, ValidationResult>
 	{
+		private readonly IClienteRepository _clienteRepository;
+
+		public ClienteCommandHandler(IClienteRepository clienteRepository)
+		{
+			_clienteRepository = clienteRepository;
+		}
+
 		public async Task<ValidationResult> Handle(RegistrarClienteCommand message, CancellationToken cancellationToken)
 		{
 			if (!message.EhValido()) return message.ValidationResult;
 
 			var cliente = new Cliente(message.Id, message.Nome, message.Email, message.Cpf);
+
+			var clienteExistente = await _clienteRepository.ObterPorCpf(cliente.Cpf.Numero);
+
+			if (clienteExistente != null)
+			{
+				AdicionaErro("Este CPF já está em uso");
+				return ValidationResult;
+			}
 
 			return message.ValidationResult;
 		}
