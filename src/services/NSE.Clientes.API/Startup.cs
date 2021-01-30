75628fd59556c7ba1087a33504456762041841fb
Phillipe Roger Souza <phillrog@hotmail.com>
2021-01-30 16:09:11 -0300

    Foi configurado mediator na api cliente

--- src/services/NSE.Clientes.API/Startup.cs
+++ src/services/NSE.Clientes.API/Startup.cs
@@ -1,3 +1,4 @@
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -35,6 +36,8 @@ namespace NSE.Clientes.API
             services.AddJwtConfiguration(Configuration);
 
             services.AppSwaggerConfiguration();
+
+            services.AddMediatR(typeof(Startup));
 
             services.RegisterServices();
         }
