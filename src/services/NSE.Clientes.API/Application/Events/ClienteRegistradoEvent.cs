73240780fffee2253576c9288522d6bea139f75f
Phillipe Roger Souza <phillrog@hotmail.com>
2021-01-31 14:57:19 -0300

    Criando o evento de registro de cliente

---
+++ src/services/NSE.Clientes.API/Application/Events/ClienteRegistradoEvent.cs
@@ -0,0 +1,24 @@
+﻿using NSE.Core.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Clientes.API.Application.Events
+{
+	public class ClienteRegistradoEvent : Event
+	{
+		public Guid Id { get; set; }
+		public string Nome { get; set; }
+		public string Email { get; set; }
+		public string Cpf { get; set; }
+		public ClienteRegistradoEvent(Guid id, string nome, string email, string cpf)
+		{
+			AggregateId = id;
+			Id = id;
+			Nome = nome;
+			Email = email;
+			Cpf = cpf;			
+		}
+	}
+}
