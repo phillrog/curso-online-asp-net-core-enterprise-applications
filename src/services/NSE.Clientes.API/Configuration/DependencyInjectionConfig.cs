969d23ceb02b553250725843ad690cd269a1b4f3
Phillipe Roger Souza <phillrog@hotmail.com>
2021-01-31 15:03:37 -0300

    Declarado no DI do evento

--- src/services/NSE.Clientes.API/Configuration/DependencyInjectionConfig.cs
+++ src/services/NSE.Clientes.API/Configuration/DependencyInjectionConfig.cs
@@ -2,6 +2,7 @@
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using NSE.Clientes.API.Application.Commands;
+using NSE.Clientes.API.Application.Events;
 using NSE.Clientes.API.Data;
 using NSE.Clientes.API.Data.Repository;
 using NSE.Clientes.API.Models;
@@ -15,6 +16,9 @@ namespace NSE.Clientes.API.Configuration
         {
             services.AddScoped<IMediatorHandler, MediatorHandler>();
             services.AddScoped<IRequestHandler<RegistrarClienteCommand, ValidationResult>, ClienteCommandHandler>();
+
+            services.AddScoped<INotificationHandler<ClienteRegistradoEvent>, ClienteEventHandler>();
+
             services.AddScoped<IClienteRepository, ClienteRepository>();
             services.AddScoped<ClientesContext>();
         }
