8e07577d68b448dd0da1581a365a54ccc89ecccd
Phillipe Roger Souza <phillrog@hotmail.com>
2021-01-31 15:23:47 -0300

    Foi criado extensão mediator para publicar eventos após o sucesso do commit do contexto

--- src/services/NSE.Clientes.API/Data/ClientesContext.cs
+++ src/services/NSE.Clientes.API/Data/ClientesContext.cs
@@ -1,41 +1,72 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using NSE.Clientes.API.Models;
 using NSE.Core.Data;
+using NSE.Core.Mediator;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace NSE.Clientes.API.Data
 {
 	public sealed class ClientesContext : DbContext, IUnitOfWork
-    {
+	{
+		private readonly IMediatorHandler _mediatorHandler;
 
-        public ClientesContext(DbContextOptions<ClientesContext> options)
-            : base(options)
-        {
-            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
-            ChangeTracker.AutoDetectChangesEnabled = false;
-        }
+		public ClientesContext(DbContextOptions<ClientesContext> options, IMediatorHandler mediatorHandler)
+			: base(options)
+		{
+			ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+			ChangeTracker.AutoDetectChangesEnabled = false;
 
-        public DbSet<Cliente> Clientes { get; set; }
-        public DbSet<Endereco> Enderecos { get; set; }
+			_mediatorHandler = mediatorHandler;
+		}
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {            
-            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
-                e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
-                property.SetColumnType("varchar(100)");
+		public DbSet<Cliente> Clientes { get; set; }
+		public DbSet<Endereco> Enderecos { get; set; }
 
-            foreach (var relationship in modelBuilder.Model.GetEntityTypes()
-                .SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+				e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+				property.SetColumnType("varchar(100)");
 
-            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ClientesContext).Assembly);
-        }
+			foreach (var relationship in modelBuilder.Model.GetEntityTypes()
+				.SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
 
-        public async Task<bool> Commit()
-        {
-            var sucesso = await base.SaveChangesAsync() > 0;
+			modelBuilder.ApplyConfigurationsFromAssembly(typeof(ClientesContext).Assembly);
+		}
 
-            return sucesso;
-        }
-    }
+		public async Task<bool> Commit()
+		{
+			var sucesso = await base.SaveChangesAsync() > 0;
+
+			if (sucesso) await _mediatorHandler.PublicarEventos(this);
+
+			return sucesso;
+		}
+	}
+
+	public static class MediatorExtension
+	{
+		public static async Task PublicarEventos<T>(this IMediatorHandler mediator, T ctx) where T : DbContext
+		{
+			var domainEntities = ctx.ChangeTracker
+				.Entries<Entity>()
+				.Where(x => x.Entity.Notificacoes != null && x.Entity.Notificacoes.Any());
+
+			var domainEvents = domainEntities
+				.SelectMany(x => x.Entity.Notificacoes)
+				.ToList();
+
+			domainEntities.ToList()
+				.ForEach(entity => entity.Entity.LimparEventos());
+
+			var tasks = domainEvents
+				.Select(async (domainEvent) =>
+				{
+					await mediator.PublicarEvento(domainEvent);
+				});
+
+			await Task.WhenAll(tasks);
+		}
+	}
 }
