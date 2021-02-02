e60f2d9d1c5e3aaa1fd07baf444c0f19f8780e9a
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-01 22:24:42 -0300

    Foi criado o cliente controller

---
+++ src/services/NSE.Clientes.API/Controllers/ClientesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSE.WebAPI.Core.Controllers;
+
+namespace NSE.Clientes.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ClientesController : MainController
+	{
+	}
+}
