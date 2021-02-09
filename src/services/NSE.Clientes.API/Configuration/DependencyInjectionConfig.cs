20bad74ea017afbbb47dcf7592130bf1ebc189f3
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-09 20:45:23 -0300

    Foi criado extensão para pegar uma seção propria chamada messagequeue

--- src/services/NSE.Clientes.API/Configuration/DependencyInjectionConfig.cs
+++ src/services/NSE.Clientes.API/Configuration/DependencyInjectionConfig.cs
@@ -6,7 +6,6 @@ using NSE.Clientes.API.Application.Events;
 using NSE.Clientes.API.Data;
 using NSE.Clientes.API.Data.Repository;
 using NSE.Clientes.API.Models;
-using NSE.Clientes.API.Services;
 using NSE.Core.Mediator;
 
 namespace NSE.Clientes.API.Configuration
@@ -22,8 +21,6 @@ namespace NSE.Clientes.API.Configuration
 
             services.AddScoped<IClienteRepository, ClienteRepository>();
             services.AddScoped<ClientesContext>();
-
-            services.AddHostedService<RegistroClienteIntegrationHandler>();
         }
     }
 }
--- src/services/NSE.Clientes.API/Startup.cs
+++ src/services/NSE.Clientes.API/Startup.cs
@@ -41,6 +41,8 @@ namespace NSE.Clientes.API
             services.AddMediatR(typeof(Startup));
 
             services.RegisterServices();
+
+            services.AddMessageBusConfiguration(Configuration);
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ClientesContext clientesContext)
