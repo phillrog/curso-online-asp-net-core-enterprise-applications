b792a7f4d891c24e7a1215730f5424709746c834
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-09 20:34:12 -0300

    Criado extensão para implementar a DI do message bus

---
+++ src/building blocks/NSE.MessageBus/DependencyInjectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NSE.MessageBus
+{
+	public static class DependencyInjectionExtensions
+	{
+		public static IServiceCollection AddMessageBus(this IServiceCollection services, string connection)
+		{
+			if (string.IsNullOrEmpty(connection)) throw new ArgumentNullException();
+
+			services.AddSingleton<IMessageBus>(new MessageBus(connection));
+
+			return services;
+		}
+	}
+}
