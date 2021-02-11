c698218ed5699a7cce62c106135a0630b78f8f71
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-11 19:44:10 -0300

    Foi criado os models

--- src/services/NSE.Carrinho.API/Models/CarrinhoCliente.cs
+++ src/services/NSE.Carrinho.API/Models/CarrinhoCliente.cs
@@ -1,6 +1,23 @@
-﻿namespace NSE.Carrinho.API.Models
+﻿using System;
+using System.Collections.Generic;
+
+namespace NSE.Carrinho.API.Models
 {
 	public class CarrinhoCliente
 	{
+		public Guid Id { get; set; }
+		public Guid ClienteId { get; set; }
+		public decimal ValorTotal { get; set; }
+		public List<CarrinhoItem> Itens { get; set; } = new List<CarrinhoItem>();
+		public CarrinhoCliente(Guid clienteId)
+		{
+			Id = Guid.NewGuid();
+			ClienteId = clienteId;
+		}
+
+		public CarrinhoCliente()
+		{
+
+		}
 	}
 }
--- src/services/NSE.Carrinho.API/Models/CarrinhoItem.cs
+++ src/services/NSE.Carrinho.API/Models/CarrinhoItem.cs
@@ -1,6 +1,20 @@
-﻿namespace NSE.Carrinho.API.Models
+﻿using System;
+
+namespace NSE.Carrinho.API.Models
 {
 	public class CarrinhoItem
 	{
+		public CarrinhoItem()
+		{
+			Id = Guid.NewGuid();
+		}
+		public Guid Id { get; set; }
+		public Guid ProdutoId { get; set; }
+		public string Nome { get; set; }
+		public int Quantidade { get; set; }
+		public decimal Valor { get; set; }
+		public string Imagem { get; set; }
+		public Guid CarrinhoId { get; set; }
+		public CarrinhoCliente CarrinhoCliente { get; set; }
 	}
 }
