efbc5adf62fbf08bb367d281fe304efdb89a1810
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-12 19:44:45 -0300

    Foi criado a model do carrinho

---
+++ src/web/NSE.WebApp.MVC/Models/CarrinhoViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NSE.WebApp.MVC.Models
+{
+    public class CarrinhoViewModel
+    {
+        public decimal ValorTotal { get; set; }
+        public List<ItemProdutoViewModel> Itens { get; set; } = new List<ItemProdutoViewModel>();
+    }
+
+    public class ItemProdutoViewModel
+    {
+        public Guid ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public int Quantidade { get; set; }
+        public decimal Valor { get; set; }
+        public string Imagem { get; set; }
+    }
+}
