124363287ff82e58365c6c1b2a0698b625ffa814
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-12 19:44:21 -0300

    Foi criado o controller mvc do carrinho

---
+++ src/web/NSE.WebApp.MVC/Controllers/CarrinhoController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NSE.WebAPI.Core.Controllers;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Controllers
+{
+	[Authorize]
+	public class CarrinhoController : MainController
+	{
+        
+        [Route("carrinho")]
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("carrinho/adicionar-item")]
+        public async Task<IActionResult> AdicionarItemCarrinho(ItemProdutoViewModel itemProduto)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("carrinho/atualizar-item")]
+        public async Task<IActionResult> AtualizarItemCarrinho(Guid produtoId, int quantidade)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("carrinho/remover-item")]
+        public async Task<IActionResult> RemoverItemCarrinho(Guid produtoId)
+        {
+            return View();
+        }
+
+    }
+}
