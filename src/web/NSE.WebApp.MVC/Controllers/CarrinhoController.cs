db65587a50b42876fff9dda891435a3e1545c48a
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-12 20:22:36 -0300

    Foi implementado o adicionar atualizar e remover

--- src/web/NSE.WebApp.MVC/Controllers/CarrinhoController.cs
+++ src/web/NSE.WebApp.MVC/Controllers/CarrinhoController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using NSE.WebAPI.Core.Controllers;
 using NSE.WebApp.MVC.Models;
 using NSE.WebApp.MVC.Services;
 using System;
@@ -31,22 +30,66 @@ namespace NSE.WebApp.MVC.Controllers
         [Route("carrinho/adicionar-item")]
         public async Task<IActionResult> AdicionarItemCarrinho(ItemProdutoViewModel itemProduto)
         {
-            return View();
+            var produto = await _catalogoService.ObterPorId(itemProduto.ProdutoId);           
+
+            ValidarItemCarrinho(produto, itemProduto.Quantidade);
+            if (!OperacaoValida()) return View("Index", await _carrinhoService.ObterCarrinho());
+
+            itemProduto.Nome = produto.Nome;
+            itemProduto.Valor = produto.Valor;
+            itemProduto.Imagem = produto.Imagem;
+
+
+            var resposta = await _carrinhoService.AdicionarItemCarrinho(itemProduto);
+
+            if (ResponsePossuiErros(resposta)) return View("Index", await _carrinhoService.ObterCarrinho());
+
+            return RedirectToAction("Index");
         }
 
         [HttpPost]
         [Route("carrinho/atualizar-item")]
         public async Task<IActionResult> AtualizarItemCarrinho(Guid produtoId, int quantidade)
         {
-            return View();
+            var produto = await _catalogoService.ObterPorId(produtoId);
+            
+            if (produto == null) AdicionarErroValidacao("Produto inexistente");
+
+            ValidarItemCarrinho(produto, quantidade);
+            if (!OperacaoValida()) return View("Index", await _carrinhoService.ObterCarrinho());
+
+            var itemProduto = new ItemProdutoViewModel { ProdutoId = produtoId, Quantidade = quantidade };
+            var resposta = await _carrinhoService.AtualizarItemCarrinho(produtoId, itemProduto);
+
+            if (ResponsePossuiErros(resposta)) return View("Index", await _carrinhoService.ObterCarrinho());
+
+            return RedirectToAction("Index");
         }
 
         [HttpPost]
         [Route("carrinho/remover-item")]
         public async Task<IActionResult> RemoverItemCarrinho(Guid produtoId)
         {
-            return View();
+            var produto = await _catalogoService.ObterPorId(produtoId);
+
+            if (produto == null)
+            {
+                AdicionarErroValidacao("Produto inexistente!");
+                return View("Index", await _carrinhoService.ObterCarrinho());
+            }
+
+            var resposta = await _carrinhoService.RemoverItemCarrinho(produtoId);
+
+            if (ResponsePossuiErros(resposta)) return View("Index", await _carrinhoService.ObterCarrinho());
+
+            return RedirectToAction("Index");
         }
 
+        private void ValidarItemCarrinho(ProdutoViewModel produto, int quantidade)
+        {
+            if (produto == null) AdicionarErroValidacao("Produto inexistente!");
+            if (quantidade < 1) AdicionarErroValidacao($"Escolha ao menos uma unidade do produto {produto.Nome}");
+            if (quantidade > produto.QuantidadeEstoque) AdicionarErroValidacao($"O produto {produto.Nome} possui {produto.QuantidadeEstoque} unidades em estoque, você selecionou {quantidade}");
+        }
     }
 }
