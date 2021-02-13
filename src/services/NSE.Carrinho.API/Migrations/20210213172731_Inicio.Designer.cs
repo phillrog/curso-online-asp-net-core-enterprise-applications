394449a990fa84401a86b032ae08c4b4930b88e9
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-13 14:35:53 -0300

    Mudança nome entidade

--- src/services/NSE.Carrinho.API/Migrations/20210213172731_Inicio.Designer.cs
+++ src/services/NSE.Carrinho.API/Migrations/20210213172731_Inicio.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NSE.Carrinho.API.Data;
 
 namespace NSE.Carrinho.API.Migrations
 {
     [DbContext(typeof(CarrinhoContext))]
-    partial class CarrinhoContextModelSnapshot : ModelSnapshot
+    [Migration("20210213172731_Inicio")]
+    partial class Inicio
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/services/NSE.Carrinho.API/Migrations/20210213172731_Inicio.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NSE.Carrinho.API.Migrations
+{
+    public partial class Inicio : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CarrinhoItens_CarrinhoClientes_CarrinhoId",
+                table: "CarrinhoItens");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CarrinhoClientes",
+                table: "CarrinhoClientes");
+
+            migrationBuilder.RenameTable(
+                name: "CarrinhoClientes",
+                newName: "CarrinhoCliente");
+
+            migrationBuilder.RenameIndex(
+                name: "IDX_CLIENTE",
+                table: "CarrinhoCliente",
+                newName: "IDX_Cliente");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CarrinhoCliente",
+                table: "CarrinhoCliente",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CarrinhoItens_CarrinhoCliente_CarrinhoId",
+                table: "CarrinhoItens",
+                column: "CarrinhoId",
+                principalTable: "CarrinhoCliente",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CarrinhoItens_CarrinhoCliente_CarrinhoId",
+                table: "CarrinhoItens");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CarrinhoCliente",
+                table: "CarrinhoCliente");
+
+            migrationBuilder.RenameTable(
+                name: "CarrinhoCliente",
+                newName: "CarrinhoClientes");
+
+            migrationBuilder.RenameIndex(
+                name: "IDX_Cliente",
+                table: "CarrinhoClientes",
+                newName: "IDX_CLIENTE");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CarrinhoClientes",
+                table: "CarrinhoClientes",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CarrinhoItens_CarrinhoClientes_CarrinhoId",
+                table: "CarrinhoItens",
+                column: "CarrinhoId",
+                principalTable: "CarrinhoClientes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
