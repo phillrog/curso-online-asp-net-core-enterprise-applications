7fee662f0ef26880f792d8f6daee0d4fe396c698
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-13 14:36:10 -0300

    Correção para pegar id

--- src/building blocks/NSE.WebAPI.Core/Usuario/ClaimsPrincipalExtensions.cs
+++ src/building blocks/NSE.WebAPI.Core/Usuario/ClaimsPrincipalExtensions.cs
@@ -12,7 +12,7 @@ namespace NSE.WebAPI.Core.Usuario
                 throw new ArgumentException(nameof(principal));
             }
 
-            var claim = principal.FindFirst("sub");
+            var claim = principal.FindFirst(ClaimTypes.NameIdentifier);
             return claim?.Value;
         }
 
