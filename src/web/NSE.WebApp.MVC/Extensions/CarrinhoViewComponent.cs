6a8cecb96efce1eec7c69404805a40a007c47a06
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-13 15:15:34 -0300

    Foi criado o componente carrinho

---
+++ src/web/NSE.WebApp.MVC/Extensions/CarrinhoViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
+using NSE.WebApp.MVC.Services;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Extensions
+{
+	public class CarrinhoViewComponent : ViewComponent
+    {
+        private readonly ICarrinhoService _carrinhoService;
+
+        public CarrinhoViewComponent(ICarrinhoService carrinhoService)
+        {
+            _carrinhoService = carrinhoService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await _carrinhoService.ObterCarrinho() ?? new CarrinhoViewModel());
+        }
+    }
+}
