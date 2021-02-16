98d527bc41f20a5ef088703a816fb26ccc676b87
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-16 14:14:06 -0300

    Foi criado o dto do carrinho

---
+++ src/api gateways/NSE.Bff.Compras/Models/CarrinhoDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace NSE.Bff.Compras.Models
+{
+	public class CarrinhoDTO
+	{
+		public decimal ValorTotal { get; set; }		
+		public decimal Desconto { get; set; }
+		public List<ItemCarrinhoDTO> Itens { get; set; } = new List<ItemCarrinhoDTO>();
+	}
+}
