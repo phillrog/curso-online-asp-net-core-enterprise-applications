c9979f72898e7b30ccb133476ed588012fb70030
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-16 14:46:49 -0300

    Foi implentado o obterporid

--- src/api gateways/NSE.Bff.Compras/Services/CatalogoService.cs
+++ src/api gateways/NSE.Bff.Compras/Services/CatalogoService.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
 using NSE.Bff.Compras.Extensions;
+using NSE.Bff.Compras.Models;
 
 namespace NSE.Bff.Compras.Services
 {
     public interface ICatalogoService
     {
+        Task<ItemProdutoDTO> ObterPorId(Guid id);
     }
 
     public class CatalogoService : Service, ICatalogoService
@@ -18,5 +22,15 @@ namespace NSE.Bff.Compras.Services
             _httpClient = httpClient;
             _httpClient.BaseAddress = new Uri(settings.Value.CatalogoUrl);
         }
+
+        public async Task<ItemProdutoDTO> ObterPorId(Guid id)
+        {
+            var response = await _httpClient.GetAsync($"/catalogo/produtos/{id}");
+
+            TratarErrosResponse(response);
+
+            return await DeserializarObjetoResponse<ItemProdutoDTO>(response);
+        }
+
     }
 }
