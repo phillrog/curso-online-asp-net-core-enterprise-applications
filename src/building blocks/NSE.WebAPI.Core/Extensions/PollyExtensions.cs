bc86671a21113b286629b6c665b17b8a046b3646
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-16 15:01:18 -0300

    Foi criado polyextensions no webapi core

---
+++ src/building blocks/NSE.WebAPI.Core/Extensions/PollyExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Polly;
+using Polly.Extensions.Http;
+using Polly.Retry;
+using System;
+using System.Net.Http;
+
+namespace NSE.WebAPI.Core.Extensions
+{
+	public static class PollyExtensions
+	{
+		public static AsyncRetryPolicy<HttpResponseMessage> EsperarTentar()
+		{
+			var retry = HttpPolicyExtensions
+				.HandleTransientHttpError()
+				.WaitAndRetryAsync(new[]
+				{
+					TimeSpan.FromSeconds(1),
+					TimeSpan.FromSeconds(5),
+					TimeSpan.FromSeconds(10),
+				}, (outcome, timespan, retryCount, context) =>
+				{
+					Console.ForegroundColor = ConsoleColor.Blue;
+					Console.WriteLine($"Tentando pela {retryCount} vez!");
+					Console.ForegroundColor = ConsoleColor.White;
+				});
+
+			return retry;
+		}
+	}
+}
