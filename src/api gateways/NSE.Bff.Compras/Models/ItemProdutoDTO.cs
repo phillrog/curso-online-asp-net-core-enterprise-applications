be47d7844faeb06a8c2778baee5fdf972785a5a7
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-16 14:11:35 -0300

    Foi criado o dto de item produto

---
+++ src/api gateways/NSE.Bff.Compras/Models/ItemProdutoDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Bff.Compras.Models
+{
+    public class ItemProdutoDTO
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Valor { get; set; }
+        public string Imagem { get; set; }
+        public int QuantidadeEstoque { get; set; }
+    }
+}
