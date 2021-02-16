4012ed44ebc676b916cf304053cdc58319e63b74
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-16 14:32:59 -0300

    Foi adicionado retorno ok no service base

--- src/api gateways/NSE.Bff.Compras/Services/Service.cs
+++ src/api gateways/NSE.Bff.Compras/Services/Service.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using NSE.Core.Communication;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
@@ -32,6 +33,11 @@ namespace NSE.Bff.Compras.Services
 
             response.EnsureSuccessStatusCode();
             return true;
+        }
+
+        protected ResponseResult RetornoOk()
+        {
+            return new ResponseResult();
         }
     }
 }
