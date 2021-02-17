bf56a5db5d3100ffb0f0dee92dae261894fd69ac
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-17 20:37:58 -0300

    COnfigurações iniciais do pedido

---
+++ src/services/NSE.Pedido.API/Configuration/SwaggerConfig.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace NSE.Pedido.API.Configuration
+{
+	public static class SwaggerConfig
+	{
+		public static void AddSwaggerConfiguration(this IServiceCollection services)
+		{
+			services.AddSwaggerGen(c =>
+
+			{
+				c.SwaggerDoc("v1", new OpenApiInfo
+				{
+					Title = "NerdStore Enterprise Pedidos API",
+					Description = "Esta API faz parte do curso ASP.NET Core Enterprise Applications.",
+					Contact = new OpenApiContact() { Name = "Phillipe", Email = "phillrog@hotmail.com" },
+					License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+				});
+
+				c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+				{
+					Description = "Insira  o token  JWT desta maneira: Bearer {seu token}",
+					Name = "Authorization",
+					Scheme = "Bearer",
+					BearerFormat = "JWT",
+					In = ParameterLocation.Header,
+					Type = SecuritySchemeType.ApiKey
+				});
+
+				c.AddSecurityRequirement(new OpenApiSecurityRequirement
+				{
+					{
+						new OpenApiSecurityScheme {
+							Reference = new OpenApiReference {
+							Type = ReferenceType.SecurityScheme,
+							Id = "Bearer"
+							}
+						},
+						new string[] { }
+					}
+				});
+			});
+
+
+
+		}
+
+		public static void UseSwaggerConfiguration(this IApplicationBuilder app)
+		{
+			app.UseSwagger();
+			app.UseSwaggerUI(c =>
+			{
+				c.SwaggerEndpoint("v1/swagger.json", "v1");
+			});
+		}
+	}
+}
---
+++ src/services/NSE.Pedido.API/Controllers/PedidoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NSE.WebAPI.Core.Controllers;
+
+namespace NSE.Pedido.API.Controllers
+{
+	[Route("api/pedido")]
+	[Authorize]
+	public class PedidoController : MainController
+	{
+		[HttpGet("healthcheck")]
+		[AllowAnonymous]
+		public IActionResult Get()
+		{
+			return Ok(new { status = "Estou vivo" });
+		}
+	}
+}
