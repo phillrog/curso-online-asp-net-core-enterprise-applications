b8163f1c804a577137bf4631e853959a47a51145
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-17 21:00:09 -0300

    Foi configurado a conexão

--- src/services/NSE.Pedido.API/Configuration/ApiConfig.cs
+++ src/services/NSE.Pedido.API/Configuration/ApiConfig.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NSE.Pedidos.Infra.Data;
 using NSE.WebAPI.Core.Identidade;
+using System;
 
 namespace NSE.Pedidos.API.Configuration
 {
@@ -11,6 +14,16 @@ namespace NSE.Pedidos.API.Configuration
     {
         public static void AddApiConfiguration(this IServiceCollection services, IConfiguration configuration)
         {
+            var conn = "";
+
+            if (Environment.GetEnvironmentVariable("CONTAINER") == "true")
+                conn = configuration.GetConnectionString("Container");
+            else
+                conn = configuration.GetConnectionString("Localhost");
+
+            services.AddDbContext<PedidosContext>(options =>
+                options.UseSqlServer(conn, m => m.MigrationsAssembly("NSE.Pedidos.Infra.Data")));
+
             services.AddControllers();
 
             services.AddCors(options =>
