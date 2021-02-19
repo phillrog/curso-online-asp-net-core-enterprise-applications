e5c0122893cbb8ba1119e5757282e73450a16f74
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-19 20:26:36 -0300

    Foi implementado o voucher no controller do mvc

--- src/web/NSE.WebApp.MVC/Models/CarrinhoViewModel.cs
+++ src/web/NSE.WebApp.MVC/Models/CarrinhoViewModel.cs
@@ -6,6 +6,9 @@ namespace NSE.WebApp.MVC.Models
     public class CarrinhoViewModel
     {
         public decimal ValorTotal { get; set; }
+        public VoucherViewModel Voucher { get; set; }
+        public bool VoucherUtilizado { get; set; }
+        public decimal Desconto { get; set; }
         public List<ItemCarrinhoViewModel> Itens { get; set; } = new List<ItemCarrinhoViewModel>();
     }
 
--- src/web/NSE.WebApp.MVC/Services/ComprasBffService.cs
+++ src/web/NSE.WebApp.MVC/Services/ComprasBffService.cs
@@ -15,7 +15,9 @@ namespace NSE.WebApp.MVC.Services
 		Task<ResponseResult> AdicionarItemCarrinho(ItemCarrinhoViewModel produto);
 		Task<ResponseResult> AtualizarItemCarrinho(Guid produtoId, ItemCarrinhoViewModel produto);
 		Task<ResponseResult> RemoverItemCarrinho(Guid produtoId);
-	}
+        Task<ResponseResult> AplicarVoucherCarrinho(string voucher);
+
+    }
 	public class ComprasBffService : Service, IComprasBffService
 	{
 		private readonly HttpClient _httpClient;
@@ -70,5 +72,17 @@ namespace NSE.WebApp.MVC.Services
 
             return RetornoOk();
         }
+
+        public async Task<ResponseResult> AplicarVoucherCarrinho(string voucher)
+        {
+            var itemContent = ObterConteudo(voucher);
+
+            var response = await _httpClient.PostAsync("/compras/carrinho/aplicar-voucher/", itemContent);
+
+            if (!TratarErrosResponse(response)) return await DeserializarObjetoResponse<ResponseResult>(response);
+
+            return RetornoOk();
+        }
+
     }
 }
