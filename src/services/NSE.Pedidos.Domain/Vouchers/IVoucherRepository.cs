924c87179eee7b242f931aa4e1029b33876bdbab
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-18 21:07:17 -0300

    Foi criado o metodo para obter o voucher na interface

--- src/services/NSE.Pedidos.Domain/Vouchers/IVoucherRepository.cs
+++ src/services/NSE.Pedidos.Domain/Vouchers/IVoucherRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace NSE.Pedidos.Domain
+﻿using System.Threading.Tasks;
+
+namespace NSE.Pedidos.Domain
 {
 	public interface IVoucherRepository 
     {
+        Task<Voucher> ObterVoucherPorCodigo(string codigo);
     }
 }
