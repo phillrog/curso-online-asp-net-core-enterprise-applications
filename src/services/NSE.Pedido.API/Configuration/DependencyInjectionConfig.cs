89a20bcf0706ea37a2ce2e27d010f13a785faba6
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-19 20:01:07 -0300

    Criando o controler para buscar voucher e configurando as DI

--- src/services/NSE.Pedido.API/Configuration/DependencyInjectionConfig.cs
+++ src/services/NSE.Pedido.API/Configuration/DependencyInjectionConfig.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using NSE.Core.Mediator;
+using NSE.Pedido.API.Application.Queries;
+using NSE.Pedidos.Domain;
+using NSE.Pedidos.Infra.Data;
+using NSE.Pedidos.Infra.Data.Repository;
 using NSE.WebAPI.Core.Usuario;
 
 namespace NSE.Pedidos.API.Configuration
@@ -9,10 +13,18 @@ namespace NSE.Pedidos.API.Configuration
     {
         public static void RegisterServices(this IServiceCollection services)
         {
+            // API
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IAspNetUser, AspNetUser>();
 
+            
+            // Application
             services.AddScoped<IMediatorHandler, MediatorHandler>();
+            services.AddScoped<IVoucherQueries, VoucherQueries>();
+
+            // Data
+            services.AddScoped<IVoucherRepository, VoucherRepository>();
+            services.AddScoped<PedidosContext>();
         }
     }
 }
---
+++ src/services/NSE.Pedido.API/Controllers/VoucherController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NSE.Pedido.API.Application.DTO;
+using NSE.Pedido.API.Application.Queries;
+using NSE.WebAPI.Core.Controllers;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace NSE.Pedido.API.Controllers
+{
+    [Route("api/voucher")]
+    [Authorize]
+    public class VoucherController : MainController
+    {
+        private readonly IVoucherQueries _voucherQueries;
+
+        public VoucherController(IVoucherQueries voucherQueries)
+        {
+            _voucherQueries = voucherQueries;
+        }
+
+        [HttpGet("/{codigo}")]
+        [ProducesResponseType(typeof(VoucherDTO), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<IActionResult> ObterPorCodigo(string codigo)
+        {
+            if (string.IsNullOrEmpty(codigo)) return NotFound();
+
+            var voucher = await _voucherQueries.ObterVoucherPorCodigo(codigo);
+
+            return voucher == null ? NotFound() : CustomResponse(voucher);
+        }
+    }
+}
