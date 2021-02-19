ff065010bbe1909fe3b21d1cc26fa40490d88d33
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-19 19:27:11 -0300

    Foi criado a query stack voucher

---
+++ src/services/NSE.Pedido.API/Application/Queries/VoucherQueries.cs
@@ -0,0 +1,36 @@
+﻿using NSE.Pedido.API.Application.DTO;
+using NSE.Pedidos.Domain;
+using System.Threading.Tasks;
+
+namespace NSE.Pedido.API.Application.Queries
+{
+	public interface IVoucherQueries
+    {
+        Task<VoucherDTO> ObterVoucherPorCodigo(string codigo);
+    }
+
+	public class VoucherQueries : IVoucherQueries
+	{
+		private readonly IVoucherRepository _voucherRepository;
+
+		public VoucherQueries(IVoucherRepository voucherRepository)
+		{
+			_voucherRepository = voucherRepository;
+		}
+
+		public async Task<VoucherDTO> ObterVoucherPorCodigo(string codigo)
+		{
+			var voucher = await _voucherRepository.ObterVoucherPorCodigo(codigo);
+
+			if (voucher == null) return null;			
+
+			return new VoucherDTO
+			{
+				Codigo = voucher.Codigo,
+				TipoDesconto = (int)voucher.TipoDesconto,
+				Percentual = voucher.Percentual,
+				ValorDesconto = voucher.ValorDesconto
+			};
+		}
+	}
+}
