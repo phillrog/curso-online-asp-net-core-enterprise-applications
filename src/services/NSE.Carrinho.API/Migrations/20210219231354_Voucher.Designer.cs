a626b72964b7c769e73e0c82960ad4df31606ac5
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-19 20:16:01 -0300

    Foi criado o migration para atualizar as novas colunas de carrinho as de voucher

--- src/services/NSE.Carrinho.API/Migrations/20210219231354_Voucher.Designer.cs
+++ src/services/NSE.Carrinho.API/Migrations/20210219231354_Voucher.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NSE.Carrinho.API.Data;
 
 namespace NSE.Carrinho.API.Migrations
 {
     [DbContext(typeof(CarrinhoContext))]
-    partial class CarrinhoContextModelSnapshot : ModelSnapshot
+    [Migration("20210219231354_Voucher")]
+    partial class Voucher
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
