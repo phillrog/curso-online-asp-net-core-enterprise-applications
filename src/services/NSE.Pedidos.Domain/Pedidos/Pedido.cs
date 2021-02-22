cc51de222908f839b850286c432fb566b9033356
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-22 19:27:15 -0300

    Foi definido os comportamentos do pedido na classe pedido

--- src/services/NSE.Pedidos.Domain/Pedidos/Pedido.cs
+++ src/services/NSE.Pedidos.Domain/Pedidos/Pedido.cs
@@ -2,6 +2,7 @@
 using NSE.Core.DomainObjects;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace NSE.Pedidos.Domain.Pedidos
 {
@@ -21,5 +22,68 @@ namespace NSE.Pedidos.Domain.Pedidos
         public Endereco Endereco { get; private set; }
         // EF Rel.
         public Voucher Voucher { get; private set; }
+
+        public Pedido(Guid clienteId, decimal valorTotal, List<PedidoItem> pedidoItems,
+            bool voucherUtilizado = false, decimal desconto = 0, Guid? voucherId = null)
+        {
+            ClienteId = clienteId;
+            ValorTotal = valorTotal;
+            _pedidoItems = pedidoItems;
+
+            Desconto = desconto;
+            VoucherUtilizado = voucherUtilizado;
+            VoucherId = voucherId;
+        }
+
+        public void AutorizarPedido()
+        {
+            PedidoStatus = PedidoStatus.Autorizado;
+        }
+
+        public void AtribuirVoucher(Voucher voucher)
+        {
+            VoucherUtilizado = true;
+            VoucherId = voucher.Id;
+            Voucher = voucher;
+        }
+
+        public void AtribuirEndereco(Endereco endereco)
+        {
+            Endereco = endereco;
+        }
+
+        public void CalcularValorPedido()
+        {
+            ValorTotal = PedidoItems.Sum(p => p.CalcularValor());
+            CalcularValorTotalDesconto();
+        }
+
+        public void CalcularValorTotalDesconto()
+        {
+            if (!VoucherUtilizado) return;
+
+            decimal desconto = 0;
+            var valor = ValorTotal;
+
+            if (Voucher.TipoDesconto == TipoDescontoVoucher.Porcentagem)
+            {
+                if (Voucher.Percentual.HasValue)
+                {
+                    desconto = (valor * Voucher.Percentual.Value) / 100;
+                    valor -= desconto;
+                }
+            }
+            else
+            {
+                if (Voucher.ValorDesconto.HasValue)
+                {
+                    desconto = Voucher.ValorDesconto.Value;
+                    valor -= desconto;
+                }
+            }
+
+            ValorTotal = valor < 0 ? 0 : valor;
+            Desconto = desconto;
+        }       
     }
 }
