02d13556054d4c5057343a12129f68278f1c5e45
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-22 19:21:40 -0300

    Foram definidos as propriedades do objeto de valor endereço do pedido

--- src/services/NSE.Pedidos.Domain/Pedidos/Endereco.cs
+++ src/services/NSE.Pedidos.Domain/Pedidos/Endereco.cs
@@ -2,5 +2,12 @@
 {
 	public class Endereco
 	{
-	}
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cep { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+    }
 }
--- src/services/NSE.Pedidos.Domain/Pedidos/Pedido.cs
+++ src/services/NSE.Pedidos.Domain/Pedidos/Pedido.cs
@@ -1,9 +1,25 @@
 ﻿using NSE.Core.Data;
 using NSE.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
 
 namespace NSE.Pedidos.Domain.Pedidos
 {
 	public class Pedido: Entity, IAggregateRoot
 	{
-	}
+        public int Codigo { get; private set; }
+        public Guid ClienteId { get; private set; }
+        public Guid? VoucherId { get; private set; }
+        public bool VoucherUtilizado { get; private set; }
+        public decimal Desconto { get; private set; }
+        public decimal ValorTotal { get; private set; }
+        public DateTime DataCadastro { get; private set; }
+        public PedidoStatus PedidoStatus { get; private set; }
+
+        private readonly List<PedidoItem> _pedidoItems;
+        public IReadOnlyCollection<PedidoItem> PedidoItems => _pedidoItems;
+        public Endereco Endereco { get; private set; }
+        // EF Rel.
+        public Voucher Voucher { get; private set; }
+    }
 }
