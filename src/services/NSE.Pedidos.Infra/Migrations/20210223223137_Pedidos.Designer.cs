09d576ff888a514e3d7a7a3cceb09d0eb8b0955e
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-23 19:32:13 -0300

    Foi adicionado o migration de pedidos para criar as entidades de pedidos

--- src/services/NSE.Pedidos.Infra/Migrations/20210223223137_Pedidos.Designer.cs
+++ src/services/NSE.Pedidos.Infra/Migrations/20210223223137_Pedidos.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NSE.Pedidos.Infra.Data;
 
 namespace NSE.Pedidos.Infra.Migrations
 {
     [DbContext(typeof(PedidosContext))]
-    partial class PedidosContextModelSnapshot : ModelSnapshot
+    [Migration("20210223223137_Pedidos")]
+    partial class Pedidos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
