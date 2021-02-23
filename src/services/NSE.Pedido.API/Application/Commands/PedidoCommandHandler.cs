213e0473ad8ca11201dbe9855d49d78382c12192
Phillipe Roger Souza <phillrog@hotmail.com>
2021-02-23 19:56:09 -0300

    Foi implementado o esqueleto do handler do comando adicionar pedido

--- src/services/NSE.Pedido.API/Application/Commands/PedidoCommandHandler.cs
+++ src/services/NSE.Pedido.API/Application/Commands/PedidoCommandHandler.cs
@@ -1,8 +1,17 @@
-﻿using NSE.Core.Messages;
+﻿using FluentValidation.Results;
+using MediatR;
+using NSE.Core.Messages;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace NSE.Pedidos.API.Application.Commands
 {
-	public class PedidoCommandHandler : CommandHandler       
-    {       
-    }
+	public class PedidoCommandHandler : CommandHandler,
+		IRequestHandler<AdicionarPedidoCommand, ValidationResult>
+	{
+		public Task<ValidationResult> Handle(AdicionarPedidoCommand message, CancellationToken cancellationToken)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
 }
