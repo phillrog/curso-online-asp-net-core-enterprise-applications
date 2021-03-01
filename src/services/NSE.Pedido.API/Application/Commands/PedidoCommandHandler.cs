6438f20894a3fdbc922c8c957d93113e06a674c5
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-01 19:40:29 -0300

    Foi imeplementado o mapeamentodo pedido paar o comando

--- src/services/NSE.Pedido.API/Application/Commands/PedidoCommandHandler.cs
+++ src/services/NSE.Pedido.API/Application/Commands/PedidoCommandHandler.cs
@@ -1,6 +1,8 @@
 ﻿using FluentValidation.Results;
 using MediatR;
 using NSE.Core.Messages;
+using NSE.Pedidos.API.Application.DTO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -9,9 +11,31 @@ namespace NSE.Pedidos.API.Application.Commands
 	public class PedidoCommandHandler : CommandHandler,
 		IRequestHandler<AdicionarPedidoCommand, ValidationResult>
 	{
-		public Task<ValidationResult> Handle(AdicionarPedidoCommand message, CancellationToken cancellationToken)
+		public async Task<ValidationResult> Handle(AdicionarPedidoCommand message, CancellationToken cancellationToken)
 		{
-			throw new System.NotImplementedException();
+			if (!message.EhValido()) return message.ValidationResult;
+
+            return null;
 		}
-	}
+
+        private NSE.Pedidos.Domain.Pedidos.Pedido MapearPedido(AdicionarPedidoCommand message)
+        {
+            var endereco = new NSE.Pedidos.Domain.Pedidos.Endereco
+            {
+                Logradouro = message.Endereco.Logradouro,
+                Numero = message.Endereco.Numero,
+                Complemento = message.Endereco.Complemento,
+                Bairro = message.Endereco.Bairro,
+                Cep = message.Endereco.Cep,
+                Cidade = message.Endereco.Cidade,
+                Estado = message.Endereco.Estado
+            };
+
+            var pedido = new NSE.Pedidos.Domain.Pedidos.Pedido(message.ClienteId, message.ValorTotal, message.PedidoItems.Select(PedidoItemDTO.ParaPedidoItem).ToList(),
+                message.VoucherUtilizado, message.Desconto);
+
+            pedido.AtribuirEndereco(endereco);
+            return pedido;
+        }
+    }
 }
