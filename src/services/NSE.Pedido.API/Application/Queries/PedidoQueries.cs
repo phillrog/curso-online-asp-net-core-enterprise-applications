7910ebbf0cbc9c6c57d2003dd2ada7121b6b8810
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-01 20:16:28 -0300

    Foi adicionado a query para pegar pedidos e implementado o dapper

--- src/services/NSE.Pedido.API/Application/Queries/PedidoQueries.cs
+++ src/services/NSE.Pedido.API/Application/Queries/PedidoQueries.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
 using NSE.Pedidos.API.Application.DTO;
 using NSE.Pedidos.Domain.Pedidos;
 
@@ -15,6 +16,18 @@ namespace NSE.Pedidos.API.Application.Queries
 
     public class PedidoQueries : IPedidoQueries
     {
+        #region Sql
+        const string _sql_Pedidos_PedidosItems = @"SELECT
+                                P.ID AS 'ProdutoId', P.CODIGO, P.VOUCHERUTILIZADO, P.DESCONTO, P.VALORTOTAL,P.PEDIDOSTATUS,
+                                P.LOGRADOURO,P.NUMERO, P.BAIRRO, P.CEP, P.COMPLEMENTO, P.CIDADE, P.ESTADO,
+                                PIT.ID AS 'ProdutoItemId',PIT.PRODUTONOME, PIT.QUANTIDADE, PIT.PRODUTOIMAGEM, PIT.VALORUNITARIO 
+                                FROM PEDIDOS P 
+                                INNER JOIN PEDIDOITEMS PIT ON P.ID = PIT.PEDIDOID 
+                                WHERE P.CLIENTEID = @clienteId 
+                                AND P.DATACADASTRO between DATEADD(minute, -3,  GETDATE()) and DATEADD(minute, 0,  GETDATE())
+                                AND P.PEDIDOSTATUS = 1 
+                                ORDER BY P.DATACADASTRO DESC";
+        #endregion
         private readonly IPedidoRepository _pedidoRepository;
 
         public PedidoQueries(IPedidoRepository pedidoRepository)
@@ -23,7 +36,10 @@ namespace NSE.Pedidos.API.Application.Queries
         }
 
         public async Task<PedidoDTO> ObterUltimoPedido(Guid clienteId)
-        {            
+        {
+            var pedido = await _pedidoRepository.ObterConexao()
+                .QueryAsync<dynamic>(_sql_Pedidos_PedidosItems, new { clienteId });
+
             return MapearPedido();
         }
 
