644716c89b9a3b8f12d2eca447ddb635802fa65a
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-01 19:33:19 -0300

    Foi implementado a validação do comando pedido

--- src/services/NSE.Pedido.API/Application/Commands/AdicionarPedidoCommand.cs
+++ src/services/NSE.Pedido.API/Application/Commands/AdicionarPedidoCommand.cs
@@ -1,4 +1,5 @@
-﻿using NSE.Core.Messages;
+﻿using FluentValidation;
+using NSE.Core.Messages;
 using NSE.Pedidos.API.Application.DTO;
 using System;
 using System.Collections.Generic;
@@ -25,5 +26,40 @@ namespace NSE.Pedidos.API.Application.Commands
         public string NomeCartao { get; set; }
         public string ExpiracaoCartao { get; set; }
         public string CvvCartao { get; set; }
+
+        public class AdicionarPedidoValidation : AbstractValidator<AdicionarPedidoCommand>
+        {
+            public AdicionarPedidoValidation()
+            {
+                RuleFor(c => c.ClienteId)
+                    .NotEqual(Guid.Empty)
+                    .WithMessage("Id do cliente inválido");
+
+                RuleFor(c => c.PedidoItems.Count)
+                    .GreaterThan(0)
+                    .WithMessage("O pedido precisa ter no mínimo 1 item");
+
+                RuleFor(c => c.ValorTotal)
+                    .GreaterThan(0)
+                    .WithMessage("Valor do pedido inválido");
+
+                RuleFor(c => c.NumeroCartao)
+                    .CreditCard()
+                    .WithMessage("Número de cartão inválido");
+
+                RuleFor(c => c.NomeCartao)
+                    .NotNull()
+                    .WithMessage("Nome do portador do cartão requerido.");
+
+                RuleFor(c => c.CvvCartao.Length)
+                    .GreaterThan(2)
+                    .LessThan(5)
+                    .WithMessage("O CVV do cartão precisa ter 3 ou 4 números.");
+
+                RuleFor(c => c.ExpiracaoCartao)
+                    .NotNull()
+                    .WithMessage("Data expiração do cartão requerida.");
+            }
+        }
     }
 }
