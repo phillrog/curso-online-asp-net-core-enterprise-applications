16ac3895c57cca5dee31d8ee13e1c640da36b16a
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-01 20:42:01 -0300

    Foi implementado ohandler para jogar a mensagem na fila

--- src/services/NSE.Pedido.API/Application/Events/PedidoEventHandler.cs
+++ src/services/NSE.Pedido.API/Application/Events/PedidoEventHandler.cs
@@ -1,14 +1,23 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
+using NSE.Core.Messages.Integration;
+using NSE.MessageBus;
 
 namespace NSE.Pedidos.API.Application.Events
 {
 	public class PedidoEventHandler : INotificationHandler<PedidoRealizadoEvent>
 	{
-		public Task Handle(PedidoRealizadoEvent notification, CancellationToken cancellationToken)
+		private readonly IMessageBus _bus;
+
+		public PedidoEventHandler(IMessageBus bus)
 		{
-			throw new System.NotImplementedException();
+			_bus = bus;
+		}
+
+		public async Task Handle(PedidoRealizadoEvent message, CancellationToken cancellationToken)
+		{
+			await _bus.PublishAsync(new PedidoRealizadoIntegrationEvent(message.ClienteId));
 		}
 	}
 }
