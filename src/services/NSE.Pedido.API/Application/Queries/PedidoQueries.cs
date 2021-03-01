e1102a9bad5b8aac04e5a3c3dc315a26b6f72ae4
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-01 20:09:04 -0300

    Foi implementado o esqueleto da query de pedidos

---
+++ src/services/NSE.Pedido.API/Application/Queries/PedidoQueries.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NSE.Pedidos.API.Application.DTO;
+using NSE.Pedidos.Domain.Pedidos;
+
+namespace NSE.Pedidos.API.Application.Queries
+{
+    public interface IPedidoQueries
+    {
+        Task<PedidoDTO> ObterUltimoPedido(Guid clienteId);
+        Task<IEnumerable<PedidoDTO>> ObterListaPorClienteId(Guid clienteId);
+    }
+
+    public class PedidoQueries : IPedidoQueries
+    {
+        private readonly IPedidoRepository _pedidoRepository;
+
+        public PedidoQueries(IPedidoRepository pedidoRepository)
+        {
+            _pedidoRepository = pedidoRepository;
+        }
+
+        public async Task<PedidoDTO> ObterUltimoPedido(Guid clienteId)
+        {            
+            return MapearPedido();
+        }
+
+        public async Task<IEnumerable<PedidoDTO>> ObterListaPorClienteId(Guid clienteId)
+        {
+            var pedidos = await _pedidoRepository.ObterListaPorClienteId(clienteId);
+
+            return pedidos.Select(PedidoDTO.ParaPedidoDTO);
+        }
+
+        private PedidoDTO MapearPedido()
+        {
+            return new PedidoDTO();            
+        }
+    }
+
+}
