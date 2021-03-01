4d0505a40aa02eebe31fb0d462f3987a3ee0d03c
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-01 20:46:24 -0300

    Foi criado o serviço que ficara escutando a fila

---
+++ src/services/NSE.Carrinho.API/Services/CarrinhoIntegrationHandler.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NSE.Carrinho.API.Services
+{
+	public class CarrinhoIntegrationHandler : BackgroundService
+	{
+		protected override Task ExecuteAsync(CancellationToken stoppingToken)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
