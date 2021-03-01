de077d6bfb1bb60767f530165fc8655388168029
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-01 19:50:38 -0300

    Foi implementado a validação para voucher se houver

--- src/services/NSE.Pedido.API/Application/Commands/PedidoCommandHandler.cs
+++ src/services/NSE.Pedido.API/Application/Commands/PedidoCommandHandler.cs
@@ -2,6 +2,8 @@
 using MediatR;
 using NSE.Core.Messages;
 using NSE.Pedidos.API.Application.DTO;
+using NSE.Pedidos.Domain;
+using NSE.Pedidos.Domain.Specs;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,9 +13,23 @@ namespace NSE.Pedidos.API.Application.Commands
 	public class PedidoCommandHandler : CommandHandler,
 		IRequestHandler<AdicionarPedidoCommand, ValidationResult>
 	{
+		private readonly IVoucherRepository _voucherRepository;
+
+		public PedidoCommandHandler(IVoucherRepository voucherRepository)
+		{
+            _voucherRepository = voucherRepository;
+        }
+
 		public async Task<ValidationResult> Handle(AdicionarPedidoCommand message, CancellationToken cancellationToken)
 		{
-			if (!message.EhValido()) return message.ValidationResult;
+            // Validação do comando
+            if (!message.EhValido()) return message.ValidationResult;
+
+            // Mapear Pedido
+            var pedido = MapearPedido(message);
+
+            // Aplicar voucher se houver
+            if (!await AplicarVoucher(message, pedido)) return ValidationResult;
 
             return null;
 		}
@@ -36,6 +52,32 @@ namespace NSE.Pedidos.API.Application.Commands
 
             pedido.AtribuirEndereco(endereco);
             return pedido;
+        }
+
+        private async Task<bool> AplicarVoucher(AdicionarPedidoCommand message, NSE.Pedidos.Domain.Pedidos.Pedido pedido)
+        {
+            if (!message.VoucherUtilizado) return true;
+
+            var voucher = await _voucherRepository.ObterVoucherPorCodigo(message.VoucherCodigo);
+            if (voucher == null)
+            {
+                AdicionarErro("O voucher informado não existe!");
+                return false;
+            }
+
+            var voucherValidation = new VoucherValidation().Validate(voucher);
+            if (!voucherValidation.IsValid)
+            {
+                voucherValidation.Errors.ToList().ForEach(m => AdicionarErro(m.ErrorMessage));
+                return false;
+            }
+
+            pedido.AtribuirVoucher(voucher);
+            voucher.DebitarQuantidade();
+
+            _voucherRepository.Atualizar(voucher);
+
+            return true;
         }
     }
 }
