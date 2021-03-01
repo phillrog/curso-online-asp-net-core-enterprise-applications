2ef07ea47b2ce8cf467c37126be30a0394c49112
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-01 20:38:20 -0300

    Foi criado o handle para disparar mensagem de pedido

---
+++ src/services/NSE.Pedido.API/Application/Events/PedidoEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace NSE.Pedidos.API.Application.Events
+{
+	public class PedidoEventHandler : INotificationHandler<PedidoRealizadoEvent>
+	{
+		public Task Handle(PedidoRealizadoEvent notification, CancellationToken cancellationToken)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
