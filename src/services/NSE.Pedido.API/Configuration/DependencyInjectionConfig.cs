0102629d8dab12dc0b0719e01a0610ac6cd2b9e5
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-01 22:25:01 -0300

    Foi criado a o evento de pedido realizado

--- src/services/NSE.Pedido.API/Configuration/DependencyInjectionConfig.cs
+++ src/services/NSE.Pedido.API/Configuration/DependencyInjectionConfig.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FluentValidation.Results;
+using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using NSE.Core.Mediator;
 using NSE.Pedido.API.Application.Queries;
+using NSE.Pedidos.API.Application.Commands;
+using NSE.Pedidos.API.Application.Events;
+using NSE.Pedidos.API.Application.Queries;
 using NSE.Pedidos.Domain;
+using NSE.Pedidos.Domain.Pedidos;
 using NSE.Pedidos.Infra.Data;
 using NSE.Pedidos.Infra.Data.Repository;
 using NSE.WebAPI.Core.Usuario;
@@ -17,12 +23,19 @@ namespace NSE.Pedidos.API.Configuration
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IAspNetUser, AspNetUser>();
 
-            
+            // Commands
+            services.AddScoped<IRequestHandler<AdicionarPedidoCommand, ValidationResult>, PedidoCommandHandler>();
+
+            // Events
+            services.AddScoped<INotificationHandler<PedidoRealizadoEvent>, PedidoEventHandler>();
+
             // Application
             services.AddScoped<IMediatorHandler, MediatorHandler>();
             services.AddScoped<IVoucherQueries, VoucherQueries>();
+            services.AddScoped<IPedidoQueries, PedidoQueries>();
 
             // Data
+            services.AddScoped<IPedidoRepository, PedidoRepository>();
             services.AddScoped<IVoucherRepository, VoucherRepository>();
             services.AddScoped<PedidosContext>();
         }
