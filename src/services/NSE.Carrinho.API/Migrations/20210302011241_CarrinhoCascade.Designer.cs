999202147e6fd5058667d1ee4db70b68a5f8015d
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-01 22:13:14 -0300

    Foi alterado o modelo de delete de carrinho para excluir o carrinho e os itens de forma cascade

--- src/services/NSE.Carrinho.API/Migrations/20210302011241_CarrinhoCascade.Designer.cs
+++ src/services/NSE.Carrinho.API/Migrations/20210302011241_CarrinhoCascade.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NSE.Carrinho.API.Data;
 
 namespace NSE.Carrinho.API.Migrations
 {
     [DbContext(typeof(CarrinhoContext))]
-    partial class CarrinhoContextModelSnapshot : ModelSnapshot
+    [Migration("20210302011241_CarrinhoCascade")]
+    partial class CarrinhoCascade
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
