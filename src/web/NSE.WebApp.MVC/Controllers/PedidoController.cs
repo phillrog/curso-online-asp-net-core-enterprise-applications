62a41eae042c921aaad0c8e7df8c39575f864abf
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-02 22:42:28 -0300

    Foi criado o controller de pedido

---
+++ src/web/NSE.WebApp.MVC/Controllers/PedidoController.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
+using NSE.WebApp.MVC.Services;
+
+namespace NSE.WebApp.MVC.Controllers
+{
+    public class PedidoController : MainController
+    {
+        private readonly IClienteService _clienteService;
+        private readonly IComprasBffService _comprasBffService;
+
+        public PedidoController(IClienteService clienteService,
+            IComprasBffService comprasBffService)
+        {
+            _clienteService = clienteService;
+            _comprasBffService = comprasBffService;
+        }
+
+        [HttpGet]
+        [Route("endereco-de-entrega")]
+        public async Task<IActionResult> EnderecoEntrega()
+        {
+            var carrinho = await _comprasBffService.ObterCarrinho();
+            if (carrinho.Itens.Count == 0) return RedirectToAction("Index", "Carrinho");
+
+            var endereco = await _clienteService.ObterEndereco();
+            var pedido = _comprasBffService.MapearParaPedido(carrinho, endereco);
+
+            return View(pedido);
+        }       
+    }
+}
