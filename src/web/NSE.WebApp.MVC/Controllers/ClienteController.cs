39e66f9d81bc0bcb5210195c08e762167439c2ae
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-02 22:27:07 -0300

    Foi criado o esqueleto do controller de cliente no mvc

---
+++ src/web/NSE.WebApp.MVC/Controllers/ClienteController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace NSE.WebApp.MVC.Controllers
+{
+	[Authorize]
+    public class ClienteController : MainController
+    {             
+       
+    }
+}
