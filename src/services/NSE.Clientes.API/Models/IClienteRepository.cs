36fbb58371feef4f7ee299d2194a30abffa52ec2
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-03 21:27:42 -0300

    Foi adicionado metodos de endereço no repositorio cliente

--- src/services/NSE.Clientes.API/Models/IClienteRepository.cs
+++ src/services/NSE.Clientes.API/Models/IClienteRepository.cs
@@ -12,5 +12,8 @@ namespace NSE.Clientes.API.Models
 
         Task<IEnumerable<Cliente>> ObterTodos();
         Task<Cliente> ObterPorCpf(string cpf);
+
+        void AdicionarEndereco(Endereco endereco);
+        Task<Endereco> ObterEnderecoPorId(Guid id);
     }
 }
