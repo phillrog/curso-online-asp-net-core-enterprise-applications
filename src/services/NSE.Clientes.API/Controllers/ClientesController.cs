e8859389770ca77b192938780b4b4a0cb034e33a
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-03 21:31:42 -0300

    Foi implementado o metodo de buscar e adicionar endereço do cliente

--- src/services/NSE.Clientes.API/Controllers/ClientesController.cs
+++ src/services/NSE.Clientes.API/Controllers/ClientesController.cs
@@ -1,26 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using NSE.Clientes.API.Application.Commands;
+using NSE.Clientes.API.Models;
 using NSE.Core.Mediator;
 using NSE.WebAPI.Core.Controllers;
-using System;
-using System.Threading.Tasks;
+using NSE.WebAPI.Core.Usuario;
 
 namespace NSE.Clientes.API.Controllers
 {
 	[Route("api/[controller]")]
 	public class ClientesController : MainController
 	{
-		private readonly IMediatorHandler _mediatorHandler;
+		private readonly IClienteRepository _clienteRepository;
+		private readonly IMediatorHandler _mediator;
+		private readonly IAspNetUser _user;
 
-		public ClientesController(IMediatorHandler mediatorHandler)
+		public ClientesController(IClienteRepository clienteRepository, IMediatorHandler mediator, IAspNetUser user)
 		{
-			_mediatorHandler = mediatorHandler;
+			_clienteRepository = clienteRepository;
+			_mediator = mediator;
+			_user = user;
 		}
+
 
 		[HttpGet("clientes")]
 		public async Task<IActionResult> Index()
 		{
-			var resultado = await _mediatorHandler.EnviarComando(new RegistrarClienteCommand ( 
+			var resultado = await _mediator.EnviarComando(new RegistrarClienteCommand ( 
 				Guid.NewGuid(),
 				"Guilherme Noah Peixoto",
 				"tesate@teste.com",
@@ -29,6 +36,21 @@ namespace NSE.Clientes.API.Controllers
 			));
 
 			return CustomResponse(resultado);
+		}
+
+		[HttpGet("endereco")]
+		public async Task<IActionResult> ObterEndereco()
+		{
+			var endereco = await _clienteRepository.ObterEnderecoPorId(_user.ObterUserId());
+
+			return endereco == null ? NotFound() : CustomResponse(endereco);
+		}
+
+		[HttpPost("endereco")]
+		public async Task<IActionResult> AdicionarEndereco(AdicionarEnderecoCommand endereco)
+		{
+			endereco.ClienteId = _user.ObterUserId();
+			return CustomResponse(await _mediator.EnviarComando(endereco));
 		}
 	}
 }
