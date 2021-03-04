8f1a196b92da02504d59d71a26c1a4d5d05a51ba
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-03 21:28:17 -0300

    Foi implementado os metodos de endereço no repositorio de cliente

--- src/services/NSE.Clientes.API/Data/Repository/ClienteRepository.cs
+++ src/services/NSE.Clientes.API/Data/Repository/ClienteRepository.cs
@@ -3,21 +3,21 @@ using NSE.Clientes.API.Models;
 using NSE.Core.Data;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace NSE.Clientes.API.Data.Repository
 {
-    public class ClienteRepository : IClienteRepository
+	public class ClienteRepository : IClienteRepository
     {
         private readonly ClientesContext _context;
-        public IUnitOfWork UnitOfWork => _context;
 
         public ClienteRepository(ClientesContext context)
         {
             _context = context;
         }
-        
+
+        public IUnitOfWork UnitOfWork => _context;
+
         public async Task<IEnumerable<Cliente>> ObterTodos()
         {
             return await _context.Clientes.AsNoTracking().ToListAsync();
@@ -31,6 +31,16 @@ namespace NSE.Clientes.API.Data.Repository
         public void Adicionar(Cliente cliente)
         {
             _context.Clientes.Add(cliente);
+        }
+
+        public async Task<Endereco> ObterEnderecoPorId(Guid id)
+        {
+            return await _context.Enderecos.FirstOrDefaultAsync(e => e.ClienteId == id);
+        }
+
+        public void AdicionarEndereco(Endereco endereco)
+        {
+            _context.Enderecos.Add(endereco);
         }
 
         public void Dispose()
