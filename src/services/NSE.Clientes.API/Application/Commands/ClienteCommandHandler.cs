2f029460edb70570962f93d8fc9614d34813f339
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-03 21:31:26 -0300

    Foi imeplementao o handler de adicionar o endereço do cliente

--- src/services/NSE.Clientes.API/Application/Commands/ClienteCommandHandler.cs
+++ src/services/NSE.Clientes.API/Application/Commands/ClienteCommandHandler.cs
@@ -8,7 +8,9 @@ using System.Threading.Tasks;
 
 namespace NSE.Clientes.API.Application.Commands
 {
-	public class ClienteCommandHandler : CommandHandler, IRequestHandler<RegistrarClienteCommand, ValidationResult>
+	public class ClienteCommandHandler : CommandHandler, 
+		IRequestHandler<RegistrarClienteCommand, ValidationResult>,
+		IRequestHandler<AdicionarEnderecoCommand, ValidationResult>
 	{
 		private readonly IClienteRepository _clienteRepository;
 
@@ -34,6 +36,16 @@ namespace NSE.Clientes.API.Application.Commands
 			_clienteRepository.Adicionar(cliente);
 
 			cliente.AdicionarEvento(new ClienteRegistradoEvent(message.Id, message.Nome, message.Email, message.Cpf));
+
+			return await PersistirDados(_clienteRepository.UnitOfWork);
+		}
+
+		public async Task<ValidationResult> Handle(AdicionarEnderecoCommand message, CancellationToken cancellationToken)
+		{
+			if (!message.EhValido()) return message.ValidationResult;
+
+			var endereco = new Endereco(message.Logradouro, message.Numero, message.Complemento, message.Bairro, message.Cep, message.Cidade, message.Estado, message.ClienteId);
+			_clienteRepository.AdicionarEndereco(endereco);
 
 			return await PersistirDados(_clienteRepository.UnitOfWork);
 		}
