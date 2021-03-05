3480d3a851388e3fede7456adbb2cdd92fc12c36
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-04 22:40:16 -0300

    Adicionado o metodo para buscar produto por id

--- src/services/NSE.Catalogo.API/Controllers/CatalogoController.cs
+++ src/services/NSE.Catalogo.API/Controllers/CatalogoController.cs
@@ -33,5 +33,11 @@ namespace NSE.Catalogo.API.Controllers
         {
             return await _produtoRepository.ObterPorId(id);
         }
+
+        [HttpGet("produtos/lista/{ids}")]
+        public async Task<IEnumerable<Produto>> ObterProdutosPorId(string ids)
+        {
+            return await _produtoRepository.ObterProdutosPorId(ids);
+        }
     }
 }
