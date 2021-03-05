073bc7f0a78930dbcd36a046e9b7e1a6be025abe
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-04 22:40:36 -0300

    Adicionado metodo para finalizar pedido

--- src/api gateways/NSE.Bff.Compras/Services/PedidoService.cs
+++ src/api gateways/NSE.Bff.Compras/Services/PedidoService.cs
@@ -1,16 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
 using NSE.Bff.Compras.Extensions;
 using NSE.Bff.Compras.Models;
+using NSE.Core.Communication;
 
 namespace NSE.Bff.Compras.Services
 {
     public interface IPedidoService
     {
         Task<VoucherDTO> ObterVoucherPorCodigo(string codigo);
+        Task<ResponseResult> FinalizarPedido(PedidoDTO pedido);
+        Task<PedidoDTO> ObterUltimoPedido();
+        Task<IEnumerable<PedidoDTO>> ObterListaPorClienteId();
     }
 
     public class PedidoService : Service, IPedidoService
@@ -32,6 +37,39 @@ namespace NSE.Bff.Compras.Services
             TratarErrosResponse(response);
 
             return await DeserializarObjetoResponse<VoucherDTO>(response);
+        }
+
+        public async Task<ResponseResult> FinalizarPedido(PedidoDTO pedido)
+        {
+            var pedidoContent = ObterConteudo(pedido);
+
+            var response = await _httpClient.PostAsync("/api/pedido/", pedidoContent);
+
+            if (!TratarErrosResponse(response)) return await DeserializarObjetoResponse<ResponseResult>(response);
+
+            return RetornoOk();
+        }
+
+        public async Task<PedidoDTO> ObterUltimoPedido()
+        {
+            var response = await _httpClient.GetAsync("/api/pedido/ultimo/");
+
+            if (response.StatusCode == HttpStatusCode.NotFound) return null;
+
+            TratarErrosResponse(response);
+
+            return await DeserializarObjetoResponse<PedidoDTO>(response);
+        }
+
+        public async Task<IEnumerable<PedidoDTO>> ObterListaPorClienteId()
+        {
+            var response = await _httpClient.GetAsync("/api/pedido/lista-cliente/");
+
+            if (response.StatusCode == HttpStatusCode.NotFound) return null;
+
+            TratarErrosResponse(response);
+
+            return await DeserializarObjetoResponse<IEnumerable<PedidoDTO>>(response);
         }
     }
 }
