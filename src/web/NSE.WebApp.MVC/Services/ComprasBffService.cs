dab1a70c7a690542a15e5912e695904fd4da3502
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-04 22:33:39 -0300

    Foi criado o metodo de finalizar pagamento e buscar pedido no bff service

--- src/web/NSE.WebApp.MVC/Services/ComprasBffService.cs
+++ src/web/NSE.WebApp.MVC/Services/ComprasBffService.cs
@@ -3,6 +3,7 @@ using NSE.Core.Communication;
 using NSE.WebApp.MVC.Extensions;
 using NSE.WebApp.MVC.Models;
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -17,6 +18,10 @@ namespace NSE.WebApp.MVC.Services
 		Task<ResponseResult> RemoverItemCarrinho(Guid produtoId);
         Task<ResponseResult> AplicarVoucherCarrinho(string voucher);
 
+        // Pedido
+        Task<ResponseResult> FinalizarPedido(PedidoTransacaoViewModel pedidoTransacao);
+        Task<PedidoViewModel> ObterUltimoPedido();
+        Task<IEnumerable<PedidoViewModel>> ObterListaPorClienteId();
         PedidoTransacaoViewModel MapearParaPedido(CarrinhoViewModel carrinho, EnderecoViewModel endereco);
 
     }
@@ -86,8 +91,39 @@ namespace NSE.WebApp.MVC.Services
             return RetornoOk();
         }
 
-		public PedidoTransacaoViewModel MapearParaPedido(CarrinhoViewModel carrinho, EnderecoViewModel endereco)
-		{
+        #region Pedido
+
+        public async Task<ResponseResult> FinalizarPedido(PedidoTransacaoViewModel pedidoTransacao)
+        {
+            var pedidoContent = ObterConteudo(pedidoTransacao);
+
+            var response = await _httpClient.PostAsync("/compras/pedido/", pedidoContent);
+
+            if (!TratarErrosResponse(response)) return await DeserializarObjetoResponse<ResponseResult>(response);
+
+            return RetornoOk();
+        }
+
+        public async Task<PedidoViewModel> ObterUltimoPedido()
+        {
+            var response = await _httpClient.GetAsync("/compras/pedido/ultimo/");
+
+            TratarErrosResponse(response);
+
+            return await DeserializarObjetoResponse<PedidoViewModel>(response);
+        }
+
+        public async Task<IEnumerable<PedidoViewModel>> ObterListaPorClienteId()
+        {
+            var response = await _httpClient.GetAsync("/compras/pedido/lista-cliente/");
+
+            TratarErrosResponse(response);
+
+            return await DeserializarObjetoResponse<IEnumerable<PedidoViewModel>>(response);
+        }
+
+        public PedidoTransacaoViewModel MapearParaPedido(CarrinhoViewModel carrinho, EnderecoViewModel endereco)
+        {
             var pedido = new PedidoTransacaoViewModel
             {
                 ValorTotal = carrinho.ValorTotal,
@@ -113,5 +149,7 @@ namespace NSE.WebApp.MVC.Services
 
             return pedido;
         }
-	}
+
+        #endregion
+    }
 }
