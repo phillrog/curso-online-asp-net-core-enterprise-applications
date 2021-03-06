ca83cac1807800211dc749d397b99887bad8eb29
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-06 14:14:42 -0300

    Correção subscribe

--- src/building blocks/NSE.MessageBus/IMessageBus.cs
+++ src/building blocks/NSE.MessageBus/IMessageBus.cs
@@ -9,13 +9,14 @@ namespace NSE.MessageBus
 	{
         bool IsConnected { get; }
         IAdvancedBus AdvancedBus { get; }
+
         void Publish<T>(T message) where T : IntegrationEvent;
 
         Task PublishAsync<T>(T message) where T : IntegrationEvent;
 
         void Subscribe<T>(string subscriptionId, Action<T> onMessage) where T : class;
 
-        Task SubscribeAsync<T>(string subscriptionId, Func<T, Task> onMessage) where T : class;
+        void SubscribeAsync<T>(string subscriptionId, Func<T, Task> onMessage) where T : class;
 
         TResponse Request<TRequest, TResponse>(TRequest request)
             where TRequest : IntegrationEvent
@@ -29,7 +30,7 @@ namespace NSE.MessageBus
             where TRequest : IntegrationEvent
             where TResponse : ResponseMessage;
 
-        Task<IDisposable> RespondAsync<TRequest, TResponse>(Func<TRequest, Task<TResponse>> responder)
+        IDisposable RespondAsync<TRequest, TResponse>(Func<TRequest, Task<TResponse>> responder)
             where TRequest : IntegrationEvent
             where TResponse : ResponseMessage;
     }
