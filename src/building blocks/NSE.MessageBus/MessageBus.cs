2d0bf8e1f2ba5e9cf53e2bc2a1fcc8396c26dbbf
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-06 14:14:55 -0300

    Correção messagebus

--- src/building blocks/NSE.MessageBus/MessageBus.cs
+++ src/building blocks/NSE.MessageBus/MessageBus.cs
@@ -1,111 +1,109 @@
-﻿using EasyNetQ;
+﻿using System;
+using System.Threading.Tasks;
+using EasyNetQ;
 using NSE.Core.Messages.Integration;
 using Polly;
 using RabbitMQ.Client.Exceptions;
-using System;
-using System.Threading.Tasks;
 
 namespace NSE.MessageBus
 {
-	public class MessageBus : IMessageBus
-	{
-		private IBus _bus;
-		private IAdvancedBus _advancedBus;
-		public readonly string _connectionString;
+    public class MessageBus : IMessageBus
+    {
+        private IBus _bus;
+        private IAdvancedBus _advancedBus;
 
-		public bool IsConnected => _bus?.Advanced.IsConnected ?? false;
+        private readonly string _connectionString;
 
-		public IAdvancedBus AdvancedBus => _bus?.Advanced; 
+        public MessageBus(string connectionString)
+        {
+            _connectionString = connectionString;
+            TryConnect();
+        }
 
-		public MessageBus(string connectionString)
-		{
-			_connectionString = connectionString;
-		}
+        public bool IsConnected => _bus?.IsConnected ?? false;
+        public IAdvancedBus AdvancedBus => _bus?.Advanced;
 
-		public void Publish<T>(T message) where T : IntegrationEvent
-		{
-			TryConnect();
-			_bus.PubSub.Publish(message);
-		}
+        public void Publish<T>(T message) where T : IntegrationEvent
+        {
+            TryConnect();
+            _bus.Publish(message);
+        }
 
-		public async Task PublishAsync<T>(T message) where T : IntegrationEvent
-		{
-			TryConnect();
-			await _bus.PubSub.PublishAsync<T>(message);
-		}
+        public async Task PublishAsync<T>(T message) where T : IntegrationEvent
+        {
+            TryConnect();
+            await _bus.PublishAsync(message);
+        }
 
-		public TResponse Request<TRequest, TResponse>(TRequest request)
-			where TRequest : IntegrationEvent
-			where TResponse : ResponseMessage
-		{
-			TryConnect();
-			return _bus.Rpc.Request<TRequest, TResponse>(request);
-		}
+        public void Subscribe<T>(string subscriptionId, Action<T> onMessage) where T : class
+        {
+            TryConnect();
+            _bus.Subscribe(subscriptionId, onMessage);
+        }
 
-		public async Task<TResponse> RequestAsync<TRequest, TResponse>(TRequest request)
-			where TRequest : IntegrationEvent
-			where TResponse : ResponseMessage
-		{
-			TryConnect();
-			return await _bus.Rpc.RequestAsync<TRequest, TResponse>(request);
-		}
+        public void SubscribeAsync<T>(string subscriptionId, Func<T, Task> onMessage) where T : class
+        {
+            TryConnect();
+            _bus.SubscribeAsync(subscriptionId, onMessage);
+        }
 
-		public IDisposable Respond<TRequest, TResponse>(Func<TRequest, TResponse> responder)
-			where TRequest : IntegrationEvent
-			where TResponse : ResponseMessage
-		{
-			TryConnect();
-			return _bus.Rpc.Respond<TRequest, TResponse>(responder);
-		}
+        public TResponse Request<TRequest, TResponse>(TRequest request) where TRequest : IntegrationEvent
+            where TResponse : ResponseMessage
+        {
+            TryConnect();
+            return _bus.Request<TRequest, TResponse>(request);
+        }
 
-		public async Task<IDisposable> RespondAsync<TRequest, TResponse>(Func<TRequest, Task<TResponse>> responder)
-			where TRequest : IntegrationEvent
-			where TResponse : ResponseMessage
-		{
-			TryConnect();
-			return await _bus.Rpc.RespondAsync<TRequest, TResponse>(responder);
-		}
+        public async Task<TResponse> RequestAsync<TRequest, TResponse>(TRequest request)
+            where TRequest : IntegrationEvent where TResponse : ResponseMessage
+        {
+            TryConnect();
+            return await _bus.RequestAsync<TRequest, TResponse>(request);
+        }
 
-		public void Subscribe<T>(string subscriptionId, Action<T> onMessage) where T : class
-		{
-			TryConnect();
-			_bus.PubSub.Subscribe<T>(subscriptionId, onMessage);
-		}
+        public IDisposable Respond<TRequest, TResponse>(Func<TRequest, TResponse> responder)
+            where TRequest : IntegrationEvent where TResponse : ResponseMessage
+        {
+            TryConnect();
+            return _bus.Respond(responder);
+        }
 
-		public async Task SubscribeAsync<T>(string subscriptionId, Func<T, Task> onMessage) where T : class
-		{
-			TryConnect();
-			await _bus.PubSub.SubscribeAsync<T>(subscriptionId, onMessage);
-		}
+        public IDisposable RespondAsync<TRequest, TResponse>(Func<TRequest, Task<TResponse>> responder)
+            where TRequest : IntegrationEvent where TResponse : ResponseMessage
+        {
+            TryConnect();
+            return _bus.RespondAsync(responder);
+        }
 
-		private void TryConnect()
-		{
-			if (IsConnected) return;
+        private void TryConnect()
+        {
+            if (IsConnected) return;
 
-			var policy = Policy.Handle<EasyNetQException>()
-				.Or<BrokerUnreachableException>()
-				.WaitAndRetry(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+            var policy = Policy.Handle<EasyNetQException>()
+                .Or<BrokerUnreachableException>()
+                .WaitAndRetry(3, retryAttempt =>
+                    TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
 
-			policy.Execute(() =>
-			{
-				_bus = RabbitHutch.CreateBus(_connectionString);
-				_advancedBus = _bus.Advanced;
-				_advancedBus.Disconnected += OnDisconect;
-			});
-		}
+            policy.Execute(() =>
+            {
+                _bus = RabbitHutch.CreateBus(_connectionString);
+                _advancedBus = _bus.Advanced;
+                _advancedBus.Disconnected += OnDisconnect;
+            });
+        }
 
-		private void OnDisconect(object sender, DisconnectedEventArgs e)
-		{
-			var policy = Policy.Handle<EasyNetQException>()
-					.Or<BrokerUnreachableException>()
-					.RetryForever();
+        private void OnDisconnect(object s, EventArgs e)
+        {
+            var policy = Policy.Handle<EasyNetQException>()
+                .Or<BrokerUnreachableException>()
+                .RetryForever();
 
-			policy.Execute(TryConnect);
-		}
+            policy.Execute(TryConnect);
+        }
 
-		public void Dispose()
-		{
-			_bus.Dispose();
-		}
-	}
+        public void Dispose()
+        {
+            _bus.Dispose();
+        }
+    }
 }
