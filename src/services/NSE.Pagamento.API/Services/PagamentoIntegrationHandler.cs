2fb8a67af4fcb7de2cea1fa8a0bd5a8d4ae83325
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-13 15:00:10 -0300

    Foi criado e implementado o backgroundservice de pagamento

---
+++ src/services/NSE.Pagamento.API/Services/PagamentoIntegrationHandler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NSE.Core.DomainObjects;
+using NSE.Core.Messages.Integration;
+using NSE.MessageBus;
+using NSE.Pagamentos.API.Models;
+
+namespace NSE.Pagamentos.API.Services
+{
+    public class PagamentoIntegrationHandler : BackgroundService
+    {
+        private readonly IMessageBus _bus;
+        private readonly IServiceProvider _serviceProvider;
+
+        public PagamentoIntegrationHandler(
+                            IServiceProvider serviceProvider,
+                            IMessageBus bus)
+        {
+            _serviceProvider = serviceProvider;
+            _bus = bus;
+        }
+
+        private void SetResponder()
+        {
+            _bus.RespondAsync<PedidoIniciadoIntegrationEvent, ResponseMessage>(async request =>
+                await AutorizarPagamento(request));
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            SetResponder();
+            return Task.CompletedTask;
+        }
+
+        private async Task<ResponseMessage> AutorizarPagamento(PedidoIniciadoIntegrationEvent message)
+        {
+            using var scope = _serviceProvider.CreateScope();
+            var pagamentoService = scope.ServiceProvider.GetRequiredService<IPagamentoService>();
+            var pagamento = new Pagamento
+            {
+                PedidoId = message.PedidoId,
+                TipoPagamento = (TipoPagamento)message.TipoPagamento,
+                Valor = message.Valor,
+                CartaoCredito = new CartaoCredito(
+                    message.NomeCartao, message.NumeroCartao, message.MesAnoVencimento, message.CVV)
+            };
+
+            var response = await pagamentoService.AutorizarPagamento(pagamento);
+
+            return response;
+        }
+    }
+}
