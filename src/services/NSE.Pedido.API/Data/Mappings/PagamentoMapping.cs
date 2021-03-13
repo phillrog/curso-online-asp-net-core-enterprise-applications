b8755402f38ed1df9541498790c2b296f0208044
Phillipe Roger Souza <phillrog@hotmail.com>
2021-03-13 14:00:38 -0300

    Foi criado o mapeamento da entidade pagamento

---
+++ src/services/NSE.Pedido.API/Data/Mappings/PagamentoMapping.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NSE.Core.DomainObjects;
+using NSE.Pagamentos.API.Models;
+
+namespace NSE.Pagamentos.API.Data.Mappings
+{
+    public class PagamentoMapping : IEntityTypeConfiguration<Pagamento>
+    {
+        public void Configure(EntityTypeBuilder<Pagamento> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            // Não grava os dados do cartão
+            builder.Ignore(c => c.CartaoCredito);
+
+            // 1 : N => Pagamento : Transacao
+            builder.HasMany(c => c.Transacoes)
+                .WithOne(c => c.Pagamento)
+                .HasForeignKey(c => c.PagamentoId);
+
+            builder.ToTable("Pagamentos");
+        }
+    }
+}
