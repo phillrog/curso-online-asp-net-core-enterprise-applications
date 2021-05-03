fbb3e9dddca7acbeda9fab1a75b842aaea94fc98
phillipe.souza <phillipe.souza@wappa.com.br>
2021-05-03 19:26:34 -0300

    COnfigurando o timer

--- src/services/NSE.Pedido.API/Services/PedidoOrquestradorIntegrationHandler.cs
+++ src/services/NSE.Pedido.API/Services/PedidoOrquestradorIntegrationHandler.cs
@@ -2,13 +2,48 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-namespace NSE.Pedido.API.Services {
-    public class PedidoOrquestradorIntegrationhandler : BackgroundService
+namespace NSE.Pedido.API.Services
+{
+    public class PedidoOrquestradorIntegrationHandler : IHostedService, IDisposable
     {
-        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        private ILogger<PedidoOrquestradorIntegrationHandler> _logger;
+        private Timer _timer;
+
+        public PedidoOrquestradorIntegrationHandler(ILogger<PedidoOrquestradorIntegrationHandler> logger)
         {
-            throw new NotImplementedException();
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Serviço de pedidos iniciado.");
+
+            _timer = new Timer(ProcessarPedidos, null, TimeSpan.Zero,
+                TimeSpan.FromSeconds(15));
+
+            return Task.CompletedTask;
+        }
+
+        private async void ProcessarPedidos(object state)
+        {
+            _logger.LogInformation("Processando o pedido");
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Serviço de pedidos finalizado.");
+
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
         }
     }
 }
