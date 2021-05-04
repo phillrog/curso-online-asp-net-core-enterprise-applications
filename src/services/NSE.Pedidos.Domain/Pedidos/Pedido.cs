0dd2fcec003ba4ac0d7ff55a341078ee1c7de7c7
phillrog <phillrog@hotmail.com>
2021-05-04 20:00:41 -0300

    FOi imeplementado mais dois estados no pedido cancelar e finalizar pedido

--- src/services/NSE.Pedidos.Domain/Pedidos/Pedido.cs
+++ src/services/NSE.Pedidos.Domain/Pedidos/Pedido.cs
@@ -43,6 +43,16 @@ namespace NSE.Pedidos.Domain.Pedidos
             PedidoStatus = PedidoStatus.Autorizado;
         }
 
+        public void CancelarPedido()
+        {
+            PedidoStatus = PedidoStatus.Cancelado;
+        }
+
+        public void FinalizarPedido()
+        {
+            PedidoStatus = PedidoStatus.Pago;
+        }
+
         public void AtribuirVoucher(Voucher voucher)
         {
             VoucherUtilizado = true;
