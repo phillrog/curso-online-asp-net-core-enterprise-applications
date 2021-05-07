2a7c730c4adcb0f3d4564747604afea57b1b6aa8
phillrog <phillrog@hotmail.com>
2021-05-07 11:20:28 -0300

    Foi criado o modelo para paginação

---
+++ src/services/NSE.Catalogo.API/Models/PagedResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Catalogo.API.Models
+{
+    public class PagedResult<T> where T : class
+    {
+        public IEnumerable<T> List { get; set; }
+        public int TotalResults { get; set; }
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public string Query { get; set; }
+    }
+}
