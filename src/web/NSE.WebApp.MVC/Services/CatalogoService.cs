7635e4eed5754deea5086da4e57ea7df187d61c6
phillrog <phillrog@hotmail.com>
2021-05-07 12:16:06 -0300

    Foi imeplementado a nova busca paginada no service

--- src/web/NSE.WebApp.MVC/Services/CatalogoService.cs
+++ src/web/NSE.WebApp.MVC/Services/CatalogoService.cs
@@ -12,7 +12,7 @@ namespace NSE.WebApp.MVC.Services
 {
     public interface ICatalogoService
     {
-        Task<IEnumerable<ProdutoViewModel>> ObterTodos();
+        Task<PagedViewModel<ProdutoViewModel>> ObterTodos(int pageSize, int pageIndex, string query = null);
         Task<ProdutoViewModel> ObterPorId(Guid id);
     }
 
@@ -45,13 +45,13 @@ namespace NSE.WebApp.MVC.Services
             return await DeserializarObjetoResponse<ProdutoViewModel>(response);
         }
 
-        public async Task<IEnumerable<ProdutoViewModel>> ObterTodos()
+        public async Task<PagedViewModel<ProdutoViewModel>> ObterTodos(int pageSize, int pageIndex, string query = null)
         {
-            var response = await _httpClient.GetAsync("catalogo/produtos/");
+            var response = await _httpClient.GetAsync($"catalogo/produtos?pageSize={pageSize}&pageIndex={pageIndex}&query={query}");
 
             TratarErrosResponse(response);
 
-            return await DeserializarObjetoResponse<IEnumerable<ProdutoViewModel>>(response);
+            return await DeserializarObjetoResponse<PagedViewModel<ProdutoViewModel>>(response);
         }
     }
 }
