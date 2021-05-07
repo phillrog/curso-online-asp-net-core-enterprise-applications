9f59353bdfd92b634cc3aac460fdbbe83cb07749
phillrog <phillrog@hotmail.com>
2021-05-07 12:15:45 -0300

    Foi criado a view de paginação

---
+++ src/web/NSE.WebApp.MVC/Models/PagedViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Models
+{
+    public class PagedViewModel<T> where T : class
+    {
+        public string ReferenceAction { get; set; }
+        public IEnumerable<T> List { get; set; }
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public string Query { get; set; }
+        public int TotalResults { get; set; }
+        public double TotalPages => Math.Ceiling((double)TotalResults / PageSize);
+    }
+}
