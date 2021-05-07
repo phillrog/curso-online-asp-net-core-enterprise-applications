4f737021edcc4c0c1fc8e84a73c50697322317df
phillrog <phillrog@hotmail.com>
2021-05-07 11:21:11 -0300

    Foi implementado a paginação com dapper no repositorio

--- src/services/NSE.Catalogo.API/Data/Repository/ProdutoRepository.cs
+++ src/services/NSE.Catalogo.API/Data/Repository/ProdutoRepository.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Dapper;
+using Microsoft.EntityFrameworkCore;
 using NSE.Catalogo.API.Models;
 using NSE.Core.Data;
 using System;
@@ -18,9 +19,31 @@ namespace NSE.Catalogo.API.Data.Repository
             _context = context;
         }
 
-        public async Task<IEnumerable<Produto>> ObterTodos()
+        public async Task<PagedResult<Produto>> ObterTodos(int pageSize, int pageIndex,  string query = null)
         {
-            return await _context.Produtos.AsNoTracking().ToListAsync();
+            var sql = @$"SELECT * FROM Produtos
+                        WHERE (@Nome IS NULL OR Nome LIKE '%' + @Nome + '%')
+                        ORDER BY [Nome]
+                        OFFSET {pageSize * (pageIndex - 1)} ROWS
+                        FETCH NEXT {pageSize} ROWS ONLY
+                        SELECT COUNT(Id) FROM Produtos
+                        WHERE (@Nome IS NULL OR Nome LIKE '%' + @Nome + '%')
+                    ";
+
+            var multi = await _context.Database.GetDbConnection()
+                .QueryMultipleAsync(sql, new { Nome = query });
+
+            var produtos = multi.Read<Produto>();
+            var total = multi.Read<int>().FirstOrDefault();
+
+            return new PagedResult<Produto>()
+            {
+                List = produtos,
+                TotalResults = total,
+                PageIndex = pageIndex,
+                PageSize = pageSize,
+                Query = query
+            };
         }
 
         public async Task<Produto> ObterPorId(Guid id)
