7a4c139f3fe8db983989d20fbaf17e2ea1facf78
phillrog <phillrog@hotmail.com>
2021-05-08 20:39:06 -0300

    Criado interface para definir uma paginação generica

--- src/web/NSE.WebApp.MVC/Models/PagedViewModel.cs
+++ src/web/NSE.WebApp.MVC/Models/PagedViewModel.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace NSE.WebApp.MVC.Models
 {
-    public class PagedViewModel<T> where T : class
+    public class PagedViewModel<T> : IPagedList where T : class
     {
         public string ReferenceAction { get; set; }
         public IEnumerable<T> List { get; set; }
@@ -14,5 +12,14 @@ namespace NSE.WebApp.MVC.Models
         public string Query { get; set; }
         public int TotalResults { get; set; }
         public double TotalPages => Math.Ceiling((double)TotalResults / PageSize);
+    }
+    public interface IPagedList
+    {
+        public string ReferenceAction { get; set; }
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public string Query { get; set; }
+        public int TotalResults { get; set; }
+        public double TotalPages { get; }
     }
 }
