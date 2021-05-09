0340524d242e622216aa292d52567d1f5348b7e5
phillrog <phillrog@hotmail.com>
2021-05-08 23:34:08 -0300

    Foi configurado a hora de experiação

--- src/services/NSE.Identidade.API/Controllers/AuthController.cs
+++ src/services/NSE.Identidade.API/Controllers/AuthController.cs
@@ -154,7 +154,7 @@ namespace NSE.Identidade.API.Controllers
 			return new UsuarioRespostaLogin
 			{
 				AccessToken = encodedToken,
-				ExpiresIn = TimeSpan.FromHours(_appSettings.ExpiracaoHoras).TotalSeconds,
+				ExpiresIn = TimeSpan.FromHours(1).TotalSeconds,
 				UsuarioToken = new UsuarioToken
 				{
 					Id = user.Id,
