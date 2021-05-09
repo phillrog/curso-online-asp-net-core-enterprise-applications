793ab7d962c73bbe6c86f766ca952d49b1407238
phillrog <phillrog@hotmail.com>
2021-05-08 23:34:53 -0300

    Foi refatorado a entidade que mapeia os config do token

--- src/building blocks/NSE.WebAPI.Core/Identidade/AppSettings.cs
+++ src/building blocks/NSE.WebAPI.Core/Identidade/AppSettings.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace NSE.WebAPI.Core.Identidade
+﻿namespace NSE.WebAPI.Core.Identidade
 {
-	public class AppSettings
+    public class AppSettings
 	{
-		public string Secret { get; set; }
-		public int ExpiracaoHoras { get; set; }
-		public string Emissor { get; set; }
-		public string ValidoEm { get; set; }
+		public string AutenticacaoJwksUrl { get; set; }
 	}
 }
