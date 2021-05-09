1b616834ffe415e7491ee647fd0254867af54ebb
phillrog <phillrog@hotmail.com>
2021-05-08 22:36:51 -0300

    Foi criado o migration da tabela que guardara os token

--- src/services/NSE.Identidade.API/Migrations/20210509011811_SecKeys.Designer.cs
+++ src/services/NSE.Identidade.API/Migrations/20210509011811_SecKeys.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NSE.Identidade.API.Data;
 
 namespace NSE.Identidade.API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210509011811_SecKeys")]
+    partial class SecKeys
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
