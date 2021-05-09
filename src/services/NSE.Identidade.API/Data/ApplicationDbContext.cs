61ec4d572a0099e25b3a35f551a2658671e0c1b5
phillrog <phillrog@hotmail.com>
2021-05-08 22:37:32 -0300

    Foi implementado o ISecurityKeyDbCOntext e sua entidade

--- src/services/NSE.Identidade.API/Data/ApplicationDbContext.cs
+++ src/services/NSE.Identidade.API/Data/ApplicationDbContext.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using NetDevPack.Security.JwtSigningCredentials;
+using NetDevPack.Security.JwtSigningCredentials.Store.EntityFrameworkCore;
 
 namespace NSE.Identidade.API.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext, ISecurityKeyContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<SecurityKeyWithPrivate> SecurityKeys { get; set; }
     }
 }
