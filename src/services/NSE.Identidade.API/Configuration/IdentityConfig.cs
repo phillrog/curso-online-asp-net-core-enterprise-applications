416ea1460e4443eb35588ca9e548269ae76a361b
phillrog <phillrog@hotmail.com>
2021-05-08 22:39:36 -0300

    Foi configurado o serviço jwks e definido o algoritmo como ES256 e definido o context do banco para gravar o token

--- src/services/NSE.Identidade.API/Configuration/IdentityConfig.cs
+++ src/services/NSE.Identidade.API/Configuration/IdentityConfig.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using NetDevPack.Security.JwtSigningCredentials;
 using NSE.Identidade.API.Data;
 using NSE.Identidade.API.Extensions;
 using NSE.WebAPI.Core.Identidade;
@@ -20,9 +21,12 @@ namespace NSE.Identidade.API.Configuration
 				conn = configuration.GetConnectionString("Container");
 			else
 				conn = configuration.GetConnectionString("Localhost");
-
+			
 			services.AddDbContext<ApplicationDbContext>(options =>
 				options.UseSqlServer(conn));
+
+			services.AddJwksManager(options => options.Algorithm = Algorithm.ES256)
+				.PersistKeysToDatabaseStore<ApplicationDbContext>();
 
 			services.AddDefaultIdentity<IdentityUser>()
 				.AddRoles<IdentityRole>()
