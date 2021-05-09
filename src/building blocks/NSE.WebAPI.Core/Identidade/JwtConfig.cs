6aac951d9566188a6f76c153470be7a68667a2e3
phillrog <phillrog@hotmail.com>
2021-05-08 23:35:35 -0300

    Foi implementado o jwt para consultar o token publico

--- src/building blocks/NSE.WebAPI.Core/Identidade/JwtConfig.cs
+++ src/building blocks/NSE.WebAPI.Core/Identidade/JwtConfig.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
+using NetDevPack.Security.JwtExtensions;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -14,31 +15,22 @@ namespace NSE.WebAPI.Core.Identidade
 		public static void AddJwtConfiguration(this IServiceCollection services, IConfiguration configuration)
 		{
 
-			var appSettingsSection = configuration.GetSection("AppSettings");
-			services.Configure<AppSettings>(appSettingsSection);
+            var appSettingsSection = configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
 
-			var appSettings = appSettingsSection.Get<AppSettings>();
-			var key = Encoding.ASCII.GetBytes(appSettings.Secret);
-			services.AddAuthentication(options => {
-				options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-				options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-			}).AddJwtBearer(beareOptions =>
-			{
-				beareOptions.RequireHttpsMetadata = true;
-				beareOptions.SaveToken = true;
-				beareOptions.TokenValidationParameters = new TokenValidationParameters
-				{
-					ValidateIssuerSigningKey = true,
-					IssuerSigningKey = new SymmetricSecurityKey(key),
-					ValidateIssuer = true,
-					ValidateAudience = true,
-					ValidAudience = appSettings.ValidoEm,
-					ValidIssuer = appSettings.Emissor
-				};
+            var appSettings = appSettingsSection.Get<AppSettings>();
 
-			});
-
-		}
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = true;
+                x.SaveToken = true;
+                x.SetJwksOptions(new JwkOptions(appSettings.AutenticacaoJwksUrl));
+            });
+        }
 
 		public static void UseJwtConfiguration(this IApplicationBuilder app)
 		{
