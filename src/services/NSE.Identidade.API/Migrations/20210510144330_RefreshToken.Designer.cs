9d7aa04b6dc9f2193b3377f6ebebc32cc4841781
phillrog <phillrog@hotmail.com>
2021-05-10 12:01:39 -0300

    Foi criado o migration do refresh token

--- src/services/NSE.Identidade.API/Migrations/20210510144330_RefreshToken.Designer.cs
+++ src/services/NSE.Identidade.API/Migrations/20210510144330_RefreshToken.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NSE.Identidade.API.Data;
 
 namespace NSE.Identidade.API.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210510144330_RefreshToken")]
+    partial class RefreshToken
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
