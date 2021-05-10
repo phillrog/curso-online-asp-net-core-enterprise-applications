75f3da0d10c2d38f8c6a6c05f810949521ba70a9
phillrog <phillrog@hotmail.com>
2021-05-10 12:03:30 -0300

    FOi criado o dbset do refresh token

--- src/services/NSE.Identidade.API/Data/ApplicationDbContext.cs
+++ src/services/NSE.Identidade.API/Data/ApplicationDbContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using NetDevPack.Security.JwtSigningCredentials;
 using NetDevPack.Security.JwtSigningCredentials.Store.EntityFrameworkCore;
+using NSE.Identidade.API.Models;
 
 namespace NSE.Identidade.API.Data
 {
@@ -9,5 +10,6 @@ namespace NSE.Identidade.API.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
         public DbSet<SecurityKeyWithPrivate> SecurityKeys { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
     }
 }
