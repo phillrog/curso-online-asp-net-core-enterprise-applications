4e76d42a98a1475fdc712814b9e1c66594917274
phillrog <phillrog@hotmail.com>
2021-05-12 19:51:43 -0300

    Foi injetado o service no controller e utilizado a busca de carrinho pelo client

--- src/api gateways/NSE.Bff.Compras/Controllers/CarrinhoController.cs
+++ src/api gateways/NSE.Bff.Compras/Controllers/CarrinhoController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using NSE.Bff.Compras.Models;
 using NSE.Bff.Compras.Services;
+using NSE.Bff.Compras.Services.gRPC;
 using NSE.WebAPI.Core.Controllers;
 
 namespace NSE.Bff.Compras.Controllers
@@ -14,30 +15,33 @@ namespace NSE.Bff.Compras.Controllers
     {
 		private readonly ICarrinhoService _carrinhoService;
 		private readonly ICatalogoService _catalogoService;
-		private readonly IPedidoService _pedidoService;
+        private readonly ICarrinhoGrpcService _carrinhoGrpcService;
+        private readonly IPedidoService _pedidoService;
 
 		public CarrinhoController(
             ICarrinhoService carrinhoService,
             ICatalogoService catalogoService,
-            IPedidoService pedidoService)
+            IPedidoService pedidoService,
+            ICarrinhoGrpcService carrinhoGrpcService)
         {
             _carrinhoService = carrinhoService;
             _catalogoService = catalogoService;
             _pedidoService = pedidoService;
+            _carrinhoGrpcService = carrinhoGrpcService;
         }
 
         [HttpGet]
         [Route("compras/carrinho")]
         public async Task<IActionResult> Index()
         {
-            return CustomResponse(await _carrinhoService.ObterCarrinho());
+            return CustomResponse(await _carrinhoGrpcService.ObterCarrinho());
         }
 
         [HttpGet]
         [Route("compras/carrinho-quantidade")]
         public async Task<int> ObterQuantidadeCarrinho()
         {
-            var quantidade = await _carrinhoService.ObterCarrinho();
+            var quantidade = await _carrinhoGrpcService.ObterCarrinho();
             return quantidade?.Itens.Sum(i => i.Quantidade) ?? 0;
         }
 
