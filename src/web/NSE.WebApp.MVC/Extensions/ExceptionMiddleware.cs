77b3d795a86afe9adf829645c905e451c4e67adb
phillrog <phillrog@hotmail.com>
2021-05-12 20:15:34 -0300

    Foi implementado o tratamento de erro vindo do grpc

--- src/web/NSE.WebApp.MVC/Extensions/ExceptionMiddleware.cs
+++ src/web/NSE.WebApp.MVC/Extensions/ExceptionMiddleware.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.Threading.Tasks;
+using Grpc.Core;
 using Microsoft.AspNetCore.Http;
 using NSE.WebApp.MVC.Services;
 using Polly.CircuitBreaker;
@@ -41,7 +42,27 @@ namespace NSE.WebApp.MVC.Extensions
             catch (BrokenCircuitException)
             {
                 HandleCircuitBreakerExceptionAsync(httpContext);
-            }            
+            }
+            catch (RpcException ex)
+            {
+                //400 Bad Request	    INTERNAL
+                //401 Unauthorized      UNAUTHENTICATED
+                //403 Forbidden         PERMISSION_DENIED
+                //404 Not Found         UNIMPLEMENTED
+
+                var statusCode = ex.StatusCode switch
+                {
+                    StatusCode.Internal => 400,
+                    StatusCode.Unauthenticated => 401,
+                    StatusCode.PermissionDenied => 403,
+                    StatusCode.Unimplemented => 404,
+                    _ => 500
+                };
+
+                var httpStatusCode = (HttpStatusCode)Enum.Parse(typeof(HttpStatusCode), statusCode.ToString());
+
+                HandleRequestExceptionAsync(httpContext, httpStatusCode);
+            }
         }
 
         private static void HandleRequestExceptionAsync(HttpContext context, HttpStatusCode statusCode)
