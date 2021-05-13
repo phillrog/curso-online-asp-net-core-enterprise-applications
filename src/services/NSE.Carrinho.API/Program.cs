6963c02d544a3749523d928d9ad204cb6f5f7110
phillrog <phillrog@hotmail.com>
2021-05-13 17:16:12 -0300

    Foi adicionado configuração do kestrel para grpc funcionar em http

--- src/services/NSE.Carrinho.API/Program.cs
+++ src/services/NSE.Carrinho.API/Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 namespace NSE.Carrinho.API
@@ -20,6 +23,13 @@ namespace NSE.Carrinho.API
 			Host.CreateDefaultBuilder(args)
 				.ConfigureWebHostDefaults(webBuilder =>
 				{
+					webBuilder.UseKestrel(opts =>
+					{
+						opts.ListenAnyIP(4004, opts => opts.Protocols = HttpProtocols.Http1);
+						opts.ListenAnyIP(5004, opts => { opts.Protocols = HttpProtocols.Http1; });
+						opts.ListenAnyIP(4014, opts => opts.Protocols = HttpProtocols.Http2);
+					});
+					
 					webBuilder.UseStartup<Startup>();
 				});
 	}
