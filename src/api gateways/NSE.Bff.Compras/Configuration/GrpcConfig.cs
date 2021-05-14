083bb1b444056659cbc73f150999583fb206f54b
phillrog <phillrog@hotmail.com>
2021-05-14 19:26:12 -0300

    Foi configurado o ssl no grpc

--- src/api gateways/NSE.Bff.Compras/Configuration/GrpcConfig.cs
+++ src/api gateways/NSE.Bff.Compras/Configuration/GrpcConfig.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using NSE.Bff.Compras.Services.gRPC;
 using NSE.Carrinho.API.Services.gRPC;
+using NSE.WebAPI.Core.Extensions;
 
 namespace NSE.Bff.Compras.Configuration
 {
@@ -16,8 +17,9 @@ namespace NSE.Bff.Compras.Configuration
 
             services.AddGrpcClient<CarrinhoCompras.CarrinhoComprasClient>(options =>
             {
-                options.Address = new Uri("http://nse.carrinho.api:4014");
-            }).AddInterceptor<GrpcServiceInterceptor>();
+                options.Address = new Uri(configuration["CarrinhoUrl"]);
+            }).AddInterceptor<GrpcServiceInterceptor>()
+            .AllowSelfSignedCertificate();
         }
     }
 }
