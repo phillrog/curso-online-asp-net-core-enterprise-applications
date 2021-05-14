38a74f1d8936f0b0b23503283edf3e5c28b86778
phillrog <phillrog@hotmail.com>
2021-05-14 18:17:17 -0300

    Foi ligado o https redirect

--- src/web/NSE.WebApp.MVC/Configurations/WebAppConfig.cs
+++ src/web/NSE.WebApp.MVC/Configurations/WebAppConfig.cs
@@ -39,7 +39,7 @@ namespace NSE.WebApp.MVC.Configurations
 			app.UseExceptionHandler("/erro/500");
 			app.UseStatusCodePagesWithRedirects("/erro/{0}");
 			app.UseHsts();
-			//app.UseHttpsRedirection();
+			app.UseHttpsRedirection();
 			app.UseStaticFiles();
 
 			app.UseRouting();
