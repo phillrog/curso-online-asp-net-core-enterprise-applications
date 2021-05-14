454168a924fb1f81525836b40b71afa5a2622db7
phillrog <phillrog@hotmail.com>
2021-05-14 19:25:42 -0300

    Foi configurado o selfsigned do certificado

--- src/api gateways/NSE.Bff.Compras/Configuration/DependencyInjectionConfig.cs
+++ src/api gateways/NSE.Bff.Compras/Configuration/DependencyInjectionConfig.cs
@@ -21,24 +21,28 @@ namespace NSE.Bff.Compras.Configuration
             services.AddHttpClient<ICatalogoService, CatalogoService>()
                 .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
                 .AddPolicyHandler(PollyExtensions.EsperarTentar())
+                .AllowSelfSignedCertificate()
                 .AddTransientHttpErrorPolicy(
                     p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)));
 
             services.AddHttpClient<ICarrinhoService, CarrinhoService>()
                 .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
                 .AddPolicyHandler(PollyExtensions.EsperarTentar())
+                .AllowSelfSignedCertificate()
                 .AddTransientHttpErrorPolicy(
                     p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)));
 
             services.AddHttpClient<IPedidoService, PedidoService>()
                 .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
                 .AddPolicyHandler(PollyExtensions.EsperarTentar())
+                .AllowSelfSignedCertificate()
                 .AddTransientHttpErrorPolicy(
                     p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)));
 
             services.AddHttpClient<IClienteService, ClienteService>()
                .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
                .AddPolicyHandler(PollyExtensions.EsperarTentar())
+               .AllowSelfSignedCertificate()
                .AddTransientHttpErrorPolicy(
                    p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)));
         }
--- src/web/NSE.WebApp.MVC/Configurations/DependencyInjectionConfig.cs
+++ src/web/NSE.WebApp.MVC/Configurations/DependencyInjectionConfig.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc.DataAnnotations;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using NSE.WebAPI.Core.Extensions;
 using NSE.WebAPI.Core.Usuario;
 using NSE.WebApp.MVC.Extensions;
 using NSE.WebApp.MVC.Services;
@@ -27,24 +28,28 @@ namespace NSE.WebApp.MVC.Configurations
 			#region HttpServices
 			services.AddHttpClient<IAutenticacaoService, AutenticacaoService>()
 				.AddPolicyHandler(PollyExtensions.EsperarTentar())
+				.AllowSelfSignedCertificate()
 				.AddTransientHttpErrorPolicy(
 					p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)));
 
 			services.AddHttpClient<ICatalogoService, CatalogoService>()
 				.AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
 				.AddPolicyHandler(PollyExtensions.EsperarTentar())
+				.AllowSelfSignedCertificate()
 				.AddTransientHttpErrorPolicy(
 					p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)));
 
 			services.AddHttpClient<IComprasBffService, ComprasBffService>()
 				.AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
 				.AddPolicyHandler(PollyExtensions.EsperarTentar())
+				.AllowSelfSignedCertificate()
 				.AddTransientHttpErrorPolicy(
 					p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)));
 
 			services.AddHttpClient<IClienteService, ClienteService>()
 				.AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
 				.AddPolicyHandler(PollyExtensions.EsperarTentar())
+				.AllowSelfSignedCertificate()
 				.AddTransientHttpErrorPolicy(
 					p => p.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)));
 
